40d57c29934f862faa6ded07bb95c44af74c5143
VP <vp@lido.fi>
2024-03-22 17:36:20 +0100

    feat: tiebreaker possible implementation

---
+++ contracts/0.8.9/oracle/Tiebreaker.sol
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2024 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.9;
+
+import { AccessControlEnumerable } from "../utils/access/AccessControlEnumerable.sol";
+import { SafeCast } from "@openzeppelin/contracts-v4.4/utils/math/SafeCast.sol";
+
+interface ILidoZKOracle {
+    function getReport(uint256 refSlot) external view returns  (
+        bool success,
+        uint256 clBalanceGwei,
+        uint256 numValidators,
+        uint256 exitedValidators
+	);
+}
+
+contract Tiebreaker is ILidoZKOracle, AccessControlEnumerable {
+    using SafeCast for uint256;
+
+    error AdminCannotBeZero();
+
+    struct Report {
+        bool success;
+        uint256 clBalanceGwei;
+        uint256 numValidators;
+        uint256 exitedValidators;
+    }
+
+    mapping(uint256 => Report) internal _reports;
+
+    constructor(address admin) {
+        if (admin == address(0)) revert AdminCannotBeZero();
+
+        _setupRole(DEFAULT_ADMIN_ROLE, admin);
+    }
+
+    function submitReport(uint256 refSlot, bool success, uint256 clBalanceGwei,
+        uint256 numValidators, uint256 exitedValidators) external
+        onlyRole(DEFAULT_ADMIN_ROLE) {
+        _reports[refSlot] = Report(success, clBalanceGwei, numValidators, exitedValidators);
+    }
+
+    function removeReport(uint256 refSlot) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        delete _reports[refSlot];
+    }
+
+    function getReport(uint256 refSlot) external view override returns (bool, uint256, uint256, uint256) {
+        Report memory report = _reports[refSlot];
+        return (report.success, report.clBalanceGwei, report.numValidators, report.exitedValidators);
+    }
+
+}
