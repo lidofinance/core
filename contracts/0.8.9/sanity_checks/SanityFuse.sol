6529148bca5bb6972a3a705b63e65a99f3f23ddd
VP <vp@lido.fi>
2024-04-03 12:37:09 +0200

    feat: optimized sanity fuse

--- contracts/0.8.9/sanity_checks/SanityFuse.sol
+++ contracts/0.8.9/sanity_checks/SanityFuse.sol
@@ -4,36 +4,63 @@ pragma solidity 0.8.9;
 
 contract SanityFuse {
 
-    address private _fuseCommittee;
-    uint256 private _expiryTimestamp;
+    event FuseBlown();
+    event FuseConsulted(bool isFuseBlown, uint256 successfulReports);
 
-    uint256 private _lastSuccessfulReports;
+    error FuseCommitteeCannotBeZero();
+    error ExpiryTimestampCannotBeInThePast();
+    error ExpiryTimestampIsTooDistantFuture();
+
+    uint8 private constant MAX_SUCCESSFUL_REPORTS = 3;
+
+    address private _fuseCommittee;
+    uint64 private _expiryTimestamp;
+
+    // 0 - unknown state, > 0 - successful reports count - 1
+    uint8 private _successfulReportsCount;
     bool private _isFuseBlown;
 
     constructor(address fuseCommittee, uint256 expiryTimestamp) {
+        if (fuseCommittee == address(0)) revert FuseCommitteeCannotBeZero();
+        if (expiryTimestamp <= block.timestamp) revert ExpiryTimestampCannotBeInThePast();
+        if (expiryTimestamp > 2**64 - 1) revert ExpiryTimestampIsTooDistantFuture();
+
         _fuseCommittee = fuseCommittee;
-        _expiryTimestamp = expiryTimestamp;
+        _expiryTimestamp = uint64(expiryTimestamp);
     }
 
     function blowFuse() external {
         require(msg.sender == _fuseCommittee, "SanityFuse: only committee can blow the fuse");
         require(block.timestamp < _expiryTimestamp, "SanityFuse: fuse is not expired yet");
-        require(_lastSuccessfulReports == 0, "SanityFuse: fuse can be blown only after unsuccessful report");
+        require(_successfulReportsCount == 1, "SanityFuse: fuse can be blown only after unsuccessful report");
         _isFuseBlown = true;
+        emit FuseBlown();
+    }
+
+    function getSuccessfulReports() public view returns (uint8) {
+        return _successfulReportsCount;
     }
 
     function consultFuse(bool succussfulReport) internal returns (bool) {
         if (block.timestamp >= _expiryTimestamp) {
             return false;
         }
+        uint8 lastSuccessfulReports = _successfulReportsCount;
         if (succussfulReport) {
-            _lastSuccessfulReports += 1;
+            if (lastSuccessfulReports < MAX_SUCCESSFUL_REPORTS + 1) {
+                lastSuccessfulReports += 1;
+                _successfulReportsCount = lastSuccessfulReports;
+            }
         } else {
-            _lastSuccessfulReports = 0;
+            if (lastSuccessfulReports != 1) {
+                lastSuccessfulReports = 1;
+                _successfulReportsCount = 1;
+            }
         }
-        if (_lastSuccessfulReports >= 3 && _isFuseBlown) {
+        if (lastSuccessfulReports >= MAX_SUCCESSFUL_REPORTS + 1 && _isFuseBlown) {
             _isFuseBlown = false;
         }
+        emit FuseConsulted(_isFuseBlown, lastSuccessfulReports);
         return _isFuseBlown;
     }
 
