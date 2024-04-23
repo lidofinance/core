0823023bfdb09ba6fc2101c0ef6207f1a0f2b83a
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-04-23 14:08:30 +0200

    chore: update scratch deploy

--- scripts/scratch/dao-deploy.sh
+++ scripts/scratch/dao-deploy.sh
@@ -29,39 +29,39 @@ function msg() {
 rm -f ${NETWORK_STATE_FILE}
 cp ${NETWORK_STATE_DEFAULTS_FILE} ${NETWORK_STATE_FILE}
 
-pnpm compile
+npm run compile
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/00-populate-deploy-artifact-from-env.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/00-populate-deploy-artifact-from-env.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/01-deploy-deposit-contract.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/01-deploy-deposit-contract.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/02-deploy-aragon-env.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/02-deploy-aragon-env.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/03-deploy-template-and-app-bases.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/03-deploy-template-and-app-bases.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/04-register-ens-domain.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/04-register-ens-domain.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/05-deploy-apm.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/05-deploy-apm.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/06-create-app-repos.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/06-create-app-repos.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/07-deploy-dao.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/07-deploy-dao.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/08-issue-tokens.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/08-issue-tokens.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/09-deploy-non-aragon-contracts.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/09-deploy-non-aragon-contracts.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/10-gate-seal.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/10-gate-seal.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/11-finalize-dao.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/11-finalize-dao.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/12-initialize-non-aragon-contracts.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/12-initialize-non-aragon-contracts.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/13-grant-roles.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/13-grant-roles.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/14-plug-curated-staking-module.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/14-plug-curated-staking-module.ts
 
-pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/15-transfer-roles.ts
+npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/15-transfer-roles.ts
 
 # TODO
-# pnpm hardhat --network $NETWORK run --no-compile scripts/scratch/steps/90-check-dao.ts
+# npm run hardhat --network $NETWORK run --no-compile scripts/scratch/steps/90-check-dao.ts
