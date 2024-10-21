f3051d80d7983aa11a081072c6d07aacdc8a6d38
Azat Serikov <azatsdev@gmail.com>
2024-10-21 14:32:21 +0500

    feat: extract fees WIP

--- contracts/0.8.25/vaults/DelegatorAlligator.sol
+++ contracts/0.8.25/vaults/DelegatorAlligator.sol
@@ -9,10 +9,23 @@ import {IStaking} from "./interfaces/IStaking.sol";
 import {ILiquid} from "./interfaces/ILiquid.sol";
 
 interface IRebalanceable {
+    function locked() external view returns (uint256);
+
+    function value() external view returns (uint256);
+
     function rebalance(uint256 _amountOfETH) external payable;
 }
 
 interface IVaultFees {
+    struct Report {
+        uint128 value;
+        int128 netCashFlow;
+    }
+
+    function lastReport() external view returns (Report memory);
+
+    function lastClaimedReport() external view returns (Report memory);
+
     function setVaultOwnerFee(uint256 _vaultOwnerFee) external;
 
     function setNodeOperatorFee(uint256 _nodeOperatorFee) external;
@@ -34,11 +47,25 @@ interface IVaultFees {
 //                            _____..'  .'
 //                           '-._____.-'
 contract DelegatorAlligator is AccessControlEnumerable {
+    error PerformanceDueUnclaimed();
+    error Zero(string);
+    error InsufficientWithdrawableAmount(uint256 withdrawable, uint256 requested);
+    error InsufficientUnlockedAmount(uint256 unlocked, uint256 requested);
+
+    uint256 private constant MAX_FEE = 10_000;
+
     bytes32 public constant MANAGER_ROLE = keccak256("Vault.DelegatorAlligator.ManagerRole");
     bytes32 public constant DEPOSITOR_ROLE = keccak256("Vault.DelegatorAlligator.DepositorRole");
     bytes32 public constant OPERATOR_ROLE = keccak256("Vault.DelegatorAlligator.OperatorRole");
 
     address payable public vault;
+
+    IVaultFees.Report public lastClaimedReport;
+
+    uint256 public managementFee;
+    uint256 public performanceFee;
+
+    uint256 public managementDue;
 
     constructor(address payable _vault, address _admin) {
         vault = _vault;
@@ -48,7 +75,30 @@ contract DelegatorAlligator is AccessControlEnumerable {
 
     /// * * * * * MANAGER FUNCTIONS * * * * * ///
 
-    function mint(address _receiver, uint256 _amountOfTokens) external payable onlyRole(MANAGER_ROLE) {
+    function setManagementFee(uint256 _managementFee) external onlyRole(MANAGER_ROLE) {
+        managementFee = _managementFee;
+    }
+
+    function setPerformanceFee(uint256 _performanceFee) external onlyRole(MANAGER_ROLE) {
+        performanceFee = _performanceFee;
+
+        if (getPerformanceDue() > 0) revert PerformanceDueUnclaimed();
+    }
+
+    function getPerformanceDue() public view returns (uint256) {
+        IVaultFees.Report memory lastReport = IVaultFees(vault).lastReport();
+
+        int128 _performanceDue = int128(lastReport.value - lastClaimedReport.value) -
+            int128(lastReport.netCashFlow - lastClaimedReport.netCashFlow);
+
+        if (_performanceDue > 0) {
+            return (uint128(_performanceDue) * performanceFee) / MAX_FEE;
+        } else {
+            return 0;
+        }
+    }
+
+    function mint(address _receiver, uint256 _amountOfTokens) public payable onlyRole(MANAGER_ROLE) {
         ILiquid(vault).mint(_receiver, _amountOfTokens);
     }
 
@@ -74,12 +124,27 @@ contract DelegatorAlligator is AccessControlEnumerable {
 
     /// * * * * * DEPOSITOR FUNCTIONS * * * * * ///
 
+    function getWithdrawableAmount() public view returns (uint256) {
+        uint256 reserved = _max(IRebalanceable(vault).locked(), managementDue + getPerformanceDue());
+        uint256 value = IRebalanceable(vault).value();
+
+        if (reserved > value) {
+            return 0;
+        }
+
+        return value - reserved;
+    }
+
     function deposit() external payable onlyRole(DEPOSITOR_ROLE) {
         IStaking(vault).deposit();
     }
 
-    function withdraw(address _receiver, uint256 _etherToWithdraw) external onlyRole(DEPOSITOR_ROLE) {
-        IStaking(vault).withdraw(_receiver, _etherToWithdraw);
+    function withdraw(address _receiver, uint256 _amount) external onlyRole(DEPOSITOR_ROLE) {
+        if (_receiver == address(0)) revert Zero("receiver");
+        if (_amount == 0) revert Zero("amount");
+        if (getWithdrawableAmount() < _amount) revert InsufficientWithdrawableAmount(getWithdrawableAmount(), _amount);
+
+        IStaking(vault).withdraw(_receiver, _amount);
     }
 
     function triggerValidatorExit(uint256 _numberOfKeys) external onlyRole(DEPOSITOR_ROLE) {
@@ -96,7 +161,30 @@ contract DelegatorAlligator is AccessControlEnumerable {
         IStaking(vault).topupValidators(_keysCount, _publicKeysBatch, _signaturesBatch);
     }
 
-    function claimNodeOperatorFee(address _receiver, bool _liquid) external onlyRole(OPERATOR_ROLE) {
-        IVaultFees(vault).claimNodeOperatorFee(_receiver, _liquid);
+    function claimPerformanceDue(address _receiver, bool _liquid) external onlyRole(OPERATOR_ROLE) {
+        if (_receiver == address(0)) revert Zero("_receiver");
+
+        uint256 due = getPerformanceDue();
+
+        if (due > 0) {
+            lastClaimedReport = IVaultFees(vault).lastReport();
+
+            if (_liquid) {
+                mint(_receiver, due);
+            } else {
+                _withdrawFeeInEther(_receiver, due);
+            }
+        }
+    }
+
+    function _withdrawFeeInEther(address _receiver, uint256 _amountOfTokens) internal {
+        int256 unlocked = int256(IRebalanceable(vault).value()) - int256(IRebalanceable(vault).locked());
+        uint256 canWithdrawFee = unlocked >= 0 ? uint256(unlocked) : 0;
+        if (canWithdrawFee < _amountOfTokens) revert InsufficientUnlockedAmount(canWithdrawFee, _amountOfTokens);
+        IStaking(vault).withdraw(_receiver, _amountOfTokens);
+    }
+
+    function _max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a > b ? a : b;
     }
 }
--- contracts/0.8.25/vaults/LiquidStakingVault.sol
+++ contracts/0.8.25/vaults/LiquidStakingVault.sol
@@ -22,14 +22,12 @@ contract LiquidStakingVault is StakingVault, ILiquid, ILockable {
     }
 
     Report public lastReport;
-    Report public lastClaimedReport;
 
     uint256 public locked;
 
     // Is direct validator depositing affects this accounting?
     int256 public netCashFlow;
 
-    uint256 nodeOperatorFee;
     uint256 vaultOwnerFee;
 
     uint256 public accumulatedVaultOwnerFee;
@@ -50,22 +48,10 @@ contract LiquidStakingVault is StakingVault, ILiquid, ILockable {
         return locked <= value();
     }
 
-    function accumulatedNodeOperatorFee() public view returns (uint256) {
-        int128 earnedRewards = int128(lastReport.value - lastClaimedReport.value) -
-            (lastReport.netCashFlow - lastClaimedReport.netCashFlow);
-
-        if (earnedRewards > 0) {
-            return (uint128(earnedRewards) * nodeOperatorFee) / MAX_FEE;
-        } else {
-            return 0;
-        }
-    }
-
     function canWithdraw() public view returns (uint256) {
-        uint256 reallyLocked = _max(locked, accumulatedNodeOperatorFee() + accumulatedVaultOwnerFee);
-        if (reallyLocked > value()) return 0;
+        if (locked > value()) return 0;
 
-        return value() - reallyLocked;
+        return value() - locked;
     }
 
     function deposit() public payable override(StakingVault) {
@@ -136,56 +122,6 @@ contract LiquidStakingVault is StakingVault, ILiquid, ILockable {
         accumulatedVaultOwnerFee += (_value * vaultOwnerFee) / 365 / MAX_FEE;
 
         emit Reported(_value, _ncf, _locked);
-    }
-
-    function setNodeOperatorFee(uint256 _nodeOperatorFee) external onlyOwner {
-        nodeOperatorFee = _nodeOperatorFee;
-
-        if (accumulatedNodeOperatorFee() > 0) revert NeedToClaimAccumulatedNodeOperatorFee();
-    }
-
-    function setVaultOwnerFee(uint256 _vaultOwnerFee) external onlyOwner {
-        vaultOwnerFee = _vaultOwnerFee;
-    }
-
-    function claimNodeOperatorFee(address _receiver, bool _liquid) external onlyOwner {
-        if (_receiver == address(0)) revert ZeroArgument("receiver");
-
-        uint256 feesToClaim = accumulatedNodeOperatorFee();
-
-        if (feesToClaim > 0) {
-            lastClaimedReport = lastReport;
-
-            if (_liquid) {
-                _mint(_receiver, feesToClaim);
-            } else {
-                _withdrawFeeInEther(_receiver, feesToClaim);
-            }
-        }
-    }
-
-    function claimVaultOwnerFee(address _receiver, bool _liquid) external onlyOwner {
-        if (_receiver == address(0)) revert ZeroArgument("receiver");
-        _mustBeHealthy();
-
-        uint256 feesToClaim = accumulatedVaultOwnerFee;
-
-        if (feesToClaim > 0) {
-            accumulatedVaultOwnerFee = 0;
-
-            if (_liquid) {
-                _mint(_receiver, feesToClaim);
-            } else {
-                _withdrawFeeInEther(_receiver, feesToClaim);
-            }
-        }
-    }
-
-    function _withdrawFeeInEther(address _receiver, uint256 _amountOfTokens) internal {
-        int256 unlocked = int256(value()) - int256(locked);
-        uint256 canWithdrawFee = unlocked >= 0 ? uint256(unlocked) : 0;
-        if (canWithdrawFee < _amountOfTokens) revert NotEnoughUnlockedEth(canWithdrawFee, _amountOfTokens);
-        _withdraw(_receiver, _amountOfTokens);
     }
 
     function _withdraw(address _receiver, uint256 _amountOfTokens) internal {
