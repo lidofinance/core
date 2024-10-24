e5eac5ead4fc088110f48047c90a87c5821c6629
Azat Serikov <azatsdev@gmail.com>
2024-10-24 14:15:24 +0500

    test: set up vault test

--- test/0.8.25/vaults/vault.test.ts
+++ test/0.8.25/vaults/vault.test.ts
@@ -5,6 +5,8 @@ import { Snapshot } from "test/suite";
 import {
   DepositContract__MockForBeaconChainDepositor,
   DepositContract__MockForBeaconChainDepositor__factory,
+  VaultHub__MockForVault,
+  VaultHub__MockForVault__factory,
 } from "typechain-types";
 import { Vault } from "typechain-types/contracts/0.8.25/vaults";
 import { Vault__factory } from "typechain-types/factories/contracts/0.8.25/vaults";
@@ -13,6 +15,7 @@ describe.only("Basic vault", async () => {
   let deployer: HardhatEthersSigner;
   let owner: HardhatEthersSigner;
 
+  let vaultHub: VaultHub__MockForVault;
   let depositContract: DepositContract__MockForBeaconChainDepositor;
   let vault: Vault;
 
@@ -21,11 +24,18 @@ describe.only("Basic vault", async () => {
   before(async () => {
     [deployer, owner] = await ethers.getSigners();
 
+    const vaultHubFactory = new VaultHub__MockForVault__factory(deployer);
+    const vaultHub = await vaultHubFactory.deploy();
+
     const depositContractFactory = new DepositContract__MockForBeaconChainDepositor__factory(deployer);
     depositContract = await depositContractFactory.deploy();
 
     const vaultFactory = new Vault__factory(owner);
-    vault = await vaultFactory.deploy(await owner.getAddress(), await depositContract.getAddress());
+    vault = await vaultFactory.deploy(
+      await owner.getAddress(),
+      await vaultHub.getAddress(),
+      await depositContract.getAddress(),
+    );
 
     expect(await vault.owner()).to.equal(await owner.getAddress());
   });
