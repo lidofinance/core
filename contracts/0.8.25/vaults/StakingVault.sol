372639b78002e0cef49a86f3726310c1a03df500
Azat Serikov <azatsdev@gmail.com>
2024-11-01 12:59:32 +0500

    feat: remove steth ref

--- contracts/0.8.25/vaults/StakingVault.sol
+++ contracts/0.8.25/vaults/StakingVault.sol
@@ -47,16 +47,13 @@ contract StakingVault is VaultBeaconChainDepositor, OwnableUpgradeable {
 
     constructor(
         address _vaultHub,
-        address _stETH,
         address _owner,
         address _beaconChainDepositContract
     ) VaultBeaconChainDepositor(_beaconChainDepositContract) {
         if (_vaultHub == address(0)) revert ZeroArgument("_vaultHub");
-        if (_stETH == address(0)) revert ZeroArgument("_stETH");
         if (_owner == address(0)) revert ZeroArgument("_owner");
 
         vaultHub = VaultHub(_vaultHub);
-        stETH = IERC20(_stETH);
         _transferOwnership(_owner);
     }
 
