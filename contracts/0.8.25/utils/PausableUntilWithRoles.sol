520f9ba042d50285700552857c434aa07a90a74c
Aleksei Potapkin <folkyatina@gmail.com>
2025-01-16 11:59:13 +0200

    docs: better comments

--- contracts/0.8.25/utils/PausableUntilWithRoles.sol
+++ contracts/0.8.25/utils/PausableUntilWithRoles.sol
@@ -20,16 +20,18 @@ abstract contract PausableUntilWithRoles is PausableUntil, AccessControlEnumerab
 
     /**
      * @notice Resume the contract
+     * @dev Reverts if contracts is not paused
+     * @dev Reverts if sender has no `RESUME_ROLE`
      */
     function resume() external onlyRole(RESUME_ROLE) {
         _resume();
     }
 
     /**
-     * @notice Pause the contract
+     * @notice Pause the contract for a specified period
      * @param _duration pause duration in seconds (use `PAUSE_INFINITELY` for unlimited)
      * @dev Reverts if contract is already paused
-     * @dev Reverts reason if sender has no `PAUSE_ROLE`
+     * @dev Reverts if sender has no `PAUSE_ROLE`
      * @dev Reverts if zero duration is passed
      */
     function pauseFor(uint256 _duration) external onlyRole(PAUSE_ROLE) {
@@ -37,7 +39,7 @@ abstract contract PausableUntilWithRoles is PausableUntil, AccessControlEnumerab
     }
 
     /**
-     * @notice Pause the contract until a specific timestamp
+     * @notice Pause the contract until a specified timestamp
      * @param _pauseUntilInclusive the last second to pause until inclusive
      * @dev Reverts if the timestamp is in the past
      * @dev Reverts if sender has no `PAUSE_ROLE`
