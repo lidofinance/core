ef1399e92c96725c5e9309e7a8504ca1392b4d58
failingtwice <azatsdev@gmail.com>
2025-01-23 17:03:39 +0500

    feat(Dashboard): add batch role methods

--- contracts/0.8.25/vaults/Dashboard.sol
+++ contracts/0.8.25/vaults/Dashboard.sol
@@ -216,6 +216,65 @@ contract Dashboard is AccessControlVoteable {
     }
 
     /**
+     * @notice Grants multiple roles to a single account.
+     * @param _account The address to which the roles will be granted.
+     * @param _roles An array of bytes32 role identifiers to be granted.
+     * @dev Performs the role admin checks internally.
+     */
+    function grantRoles(address _account, bytes32[] memory _roles) external {
+        if (_account == address(0)) revert ZeroArgument("_account");
+        if (_roles.length == 0) revert ZeroArgument("_roles");
+
+        for (uint256 i = 0; i < _roles.length; i++) {
+            grantRole(_roles[i], _account);
+        }
+    }
+
+    /**
+     * @notice Batch-grants a single role to a single account.
+     * @param _accounts An array of addresses to which the roles will be granted.
+     * @param _roles An array of bytes32 role identifiers to be granted.
+     */
+    function grantRoles(address[] memory _accounts, bytes32[] memory _roles) external {
+        if (_accounts.length == 0) revert ZeroArgument("_accounts");
+        if (_roles.length == 0) revert ZeroArgument("_roles");
+        if (_accounts.length != _roles.length) revert UnequalLengths();
+
+        for (uint256 i = 0; i < _accounts.length; i++) {
+            grantRole(_roles[i], _accounts[i]);
+        }
+    }
+
+    /**
+     * @notice Revokes multiple roles from a single account.
+     * @param _account The address from which the roles will be revoked.
+     * @param _roles An array of bytes32 role identifiers to be revoked.
+     */
+    function revokeRoles(address _account, bytes32[] memory _roles) external {
+        if (_account == address(0)) revert ZeroArgument("_account");
+        if (_roles.length == 0) revert ZeroArgument("_roles");
+
+        for (uint256 i = 0; i < _roles.length; i++) {
+            revokeRole(_roles[i], _account);
+        }
+    }
+
+    /**
+     * @notice Batch-revokes a single role from a single account.
+     * @param _accounts An array of addresses from which the roles will be revoked.
+     * @param _roles An array of bytes32 role identifiers to be revoked.
+     */
+    function revokeRoles(address[] memory _accounts, bytes32[] memory _roles) external {
+        if (_accounts.length == 0) revert ZeroArgument("_accounts");
+        if (_roles.length == 0) revert ZeroArgument("_roles");
+        if (_accounts.length != _roles.length) revert UnequalLengths();
+
+        for (uint256 i = 0; i < _accounts.length; i++) {
+            revokeRole(_roles[i], _accounts[i]);
+        }
+    }
+
+    /**
      * @notice Transfers ownership of the staking vault to a new owner.
      * @param _newOwner Address of the new owner.
      */
