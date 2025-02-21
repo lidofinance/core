50f8045ca2cf8c46e41c23a7c7831f2fe22a915d
Maksim Kuraian <kuraian@proton.me>
2025-02-21 16:54:17 +0100

    feat: add cl proof verifier

---
+++ test/0.8.25/beaconBlockRoot.ts
@@ -0,0 +1,23 @@
+import { impersonate } from "lib";
+
+// Address of the Beacon Block Storage contract, which exposes beacon chain roots.
+// This corresponds to `BEACON_ROOTS_ADDRESS` as specified in EIP-4788.
+export const BEACON_BLOCK_STORAGE_CONTRACT = "0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02";
+
+export const updateBeaconBlockRoot = async (root: string): Promise<number> => {
+  const beaconRootUpdater = await impersonate(
+    "0xfffffffffffffffffffffffffffffffffffffffe",
+    999999999999999999999999999n,
+  );
+
+  const transaction = await beaconRootUpdater.sendTransaction({
+    to: BEACON_BLOCK_STORAGE_CONTRACT,
+    value: 0,
+    data: root,
+  });
+
+  const blockDetails = await transaction.getBlock();
+  if (!blockDetails) throw new Error("Failed to retrieve block details.");
+
+  return blockDetails.timestamp;
+};
