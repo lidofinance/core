67c7d88001e357f9851f475ed007550722f0bb12
Artyom Veremeenko <arwer13@gmail.com>
2025-03-14 15:56:17 +0300

    fix: add workaround to fix forking forks (e.g. hardhat forking hardhat)

--- test/hooks/index.ts
+++ test/hooks/index.ts
@@ -1,11 +1,22 @@
 import * as Mocha from "mocha";
 
+import { mine } from "@nomicfoundation/hardhat-network-helpers";
+
 import "./assertion/revertedWithOZAccessControlError";
 
-/**
- * This is used to add custom assertions to the Chai assertion library in the test suite when it's run in parallel mode.
- */
-export const mochaRootHooks = {
+export const mochaRootHooks: Mocha.RootHookObject = {
+  /**
+   * This mine before all tests is to fix an error "No known hardfork for execution on historical block"
+   * when forking other fork e.g. hardhat forking hardhat
+   * See https://github.com/NomicFoundation/hardhat/issues/5511
+   */
+  async beforeAll() {
+    await mine();
+  },
+
+  /**
+   * This is used to add custom assertions to the Chai assertion library in the test suite when it's run in parallel mode.
+   */
   beforeEach(done: Mocha.Done) {
     done();
   },
