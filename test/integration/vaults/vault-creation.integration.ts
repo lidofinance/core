2061b7ba2ff90ed361f103034b333ec5c981f0b4
cakeinpanic <cakeinpanic@gmail.com>
2025-03-14 16:23:39 +0400

    test: add vault creation tests

---
+++ test/integration/vaults/vault-creation.integration.ts
@@ -0,0 +1,236 @@
+import { expect } from "chai";
+import { ContractRunner, ContractTransactionReceipt } from "ethers";
+import { ethers } from "hardhat";
+
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
+import { Delegation, StakingVault } from "typechain-types";
+
+import { days, ether, impersonate } from "lib";
+import { getProtocolContext, ProtocolContext } from "lib/protocol";
+
+import { Snapshot, Tracing } from "test/suite";
+
+import { getRandomSigners } from "../../../lib/protocol/helpers/get-random-signers";
+
+const SAMPLE_PUBKEY = "0x" + "ab".repeat(48);
+const VAULT_OWNER_FEE = 1_00n; // 1% AUM owner fee
+const VAULT_NODE_OPERATOR_FEE = 3_00n; // 3% node operator fee
+
+const reserveRatio = 10_00n; // 10% of ETH allocation as reserve
+const rebalanceThreshold = 8_00n; // 8% is a threshold to force rebalance on the vault
+
+describe("Scenario: Vault creation", () => {
+  let ctx: ProtocolContext;
+
+  let delegation: Delegation;
+  let stakingVault: StakingVault;
+  let owner: HardhatEthersSigner,
+    nodeOperatorManager: HardhatEthersSigner,
+    funder: HardhatEthersSigner,
+    withdrawer: HardhatEthersSigner,
+    minter: HardhatEthersSigner,
+    burner: HardhatEthersSigner,
+    assetRecoverer: HardhatEthersSigner,
+    rebalancer: HardhatEthersSigner,
+    depositPausers: HardhatEthersSigner,
+    depositResumers: HardhatEthersSigner,
+    validatorExitRequesters: HardhatEthersSigner,
+    validatorWithdrawalTriggerers: HardhatEthersSigner,
+    disconnecters: HardhatEthersSigner,
+    curatorFeeSetters: HardhatEthersSigner,
+    curatorFeeClaimers: HardhatEthersSigner,
+    nodeOperatorFeeClaimers: HardhatEthersSigner,
+    stranger: HardhatEthersSigner;
+
+  let allRoles: HardhatEthersSigner[];
+
+  let snapshot: string;
+
+  before(async () => {
+    ctx = await getProtocolContext();
+
+    const { depositSecurityModule, stakingVaultFactory } = ctx.contracts;
+    await depositSecurityModule.DEPOSIT_CONTRACT();
+
+    allRoles = await getRandomSigners(20);
+    [
+      owner,
+      nodeOperatorManager,
+      funder,
+      withdrawer,
+      minter,
+      burner,
+      assetRecoverer,
+      rebalancer,
+      depositPausers,
+      depositResumers,
+      validatorExitRequesters,
+      validatorWithdrawalTriggerers,
+      disconnecters,
+      curatorFeeSetters,
+      curatorFeeClaimers,
+      nodeOperatorFeeClaimers,
+      stranger,
+    ] = allRoles;
+
+    // Owner can create a vault with operator as a node operator
+    const deployTx = await stakingVaultFactory.connect(owner).createVaultWithDelegation(
+      {
+        defaultAdmin: owner,
+        nodeOperatorManager: nodeOperatorManager,
+        curatorFeeBP: VAULT_OWNER_FEE,
+        assetRecoverer: assetRecoverer,
+        nodeOperatorFeeBP: VAULT_NODE_OPERATOR_FEE,
+        confirmExpiry: days(7n),
+        funders: [funder],
+        withdrawers: [withdrawer],
+        minters: [minter],
+        burners: [burner],
+        rebalancers: [rebalancer],
+        depositPausers: [depositPausers],
+        depositResumers: [depositResumers],
+        validatorExitRequesters: [validatorExitRequesters],
+        validatorWithdrawalTriggerers: [validatorWithdrawalTriggerers],
+        disconnecters: [disconnecters],
+        curatorFeeSetters: [curatorFeeSetters],
+        curatorFeeClaimers: [curatorFeeClaimers],
+        nodeOperatorFeeClaimers: [nodeOperatorFeeClaimers],
+      },
+      "0x",
+    );
+    const createVaultTxReceipt = (await deployTx.wait()) as ContractTransactionReceipt;
+    const createVaultEvents = ctx.getEvents(createVaultTxReceipt, "VaultCreated");
+
+    expect(createVaultEvents.length).to.equal(1n);
+
+    stakingVault = await ethers.getContractAt("StakingVault", createVaultEvents[0].args?.vault);
+    delegation = await ethers.getContractAt("Delegation", createVaultEvents[0].args?.owner);
+
+    snapshot = await Snapshot.take();
+  });
+
+  after(async () => await Snapshot.restore(snapshot));
+
+  async function connectToHub() {
+    const { vaultHub, lido } = ctx.contracts;
+    const treasuryFeeBP = 5_00n; // 5% of the treasury fee
+
+    const agentSigner = await ctx.getSigner("agent");
+    const votingSigner = await ctx.getSigner("voting");
+    await lido.connect(votingSigner).setMaxExternalRatioBP(20_00n);
+    // only equivalent of 10.0% of TVL can be minted as stETH on the vault
+    const shareLimit = (await lido.getTotalShares()) / 10n; // 10% of total shares
+
+    await vaultHub
+      .connect(agentSigner)
+      .connectVault(stakingVault, shareLimit, reserveRatio, rebalanceThreshold, treasuryFeeBP);
+  }
+
+  async function disconnectFromHub() {
+    const { vaultHub } = ctx.contracts;
+    const agentSigner = await ctx.getSigner("agent");
+
+    await vaultHub.connect(agentSigner).disconnect(stakingVault);
+  }
+
+  it("Allows to fund an withdraw funds for dedicated roles", async () => {
+    expect(await delegation.connect(funder).fund({ value: 2n })).to.be.ok;
+    expect(await delegation.connect(owner).withdrawableEther()).to.equal(2n);
+    expect(await delegation.connect(withdrawer).withdraw(stranger, 2n)).to.be.ok;
+    expect(await delegation.connect(owner).withdrawableEther()).to.equal(0);
+  });
+
+  it("Allows to pause/resume deposits to validators", async () => {
+    expect(await delegation.connect(depositPausers).pauseBeaconChainDeposits()).to.be.ok;
+    expect(await delegation.connect(depositResumers).resumeBeaconChainDeposits()).to.be.ok;
+  });
+
+  it("Allows to force / Ask Node Operator to withdraw funds from validator(s)", async () => {
+    const vaultOwnerAddress = await stakingVault.owner();
+    const vaultOwner: ContractRunner = await impersonate(vaultOwnerAddress, ether("10000"));
+    expect(await stakingVault.connect(vaultOwner).requestValidatorExit(SAMPLE_PUBKEY)).to.be.ok;
+    Tracing.enable();
+    // todo: why it reverts with error WithdrawalFeeInvalidData()?
+    expect(
+      await stakingVault
+        .connect(vaultOwner)
+        .triggerValidatorWithdrawal(SAMPLE_PUBKEY, [ether("1")], vaultOwnerAddress, { value: 1n }),
+    ).to.be.ok;
+  });
+
+  describe("Allows actions only after connecting to Hub", () => {
+    it("Allows to mint stEth", async () => {
+      const { vaultHub } = ctx.contracts;
+
+      await expect(delegation.connect(minter).mintStETH(stranger, 1n)).to.be.revertedWithCustomError(
+        vaultHub,
+        "NotConnectedToHub",
+      );
+      await connectToHub();
+
+      await expect(delegation.connect(minter).mintStETH(stranger, 1n)).to.not.be.revertedWithCustomError(
+        vaultHub,
+        "NotConnectedToHub",
+      );
+      await disconnectFromHub();
+    });
+
+    it.skip("to burn stEth ", async () => {
+      const { vaultHub } = ctx.contracts;
+      Tracing.enable();
+
+      // todo: why it fails with ALLOWANCE_EXCEEDED before NotConnectedToHub? How to fix it?
+      await expect(delegation.connect(burner).burnStETH(1n)).to.be.revertedWithCustomError(
+        vaultHub,
+        "NotConnectedToHub",
+      );
+      await connectToHub();
+
+      await expect(delegation.connect(burner).burnStETH(1n)).to.not.be.revertedWithCustomError(
+        vaultHub,
+        "NotConnectedToHub",
+      );
+      await disconnectFromHub();
+    });
+
+    it.skip("to claim Curator's fee", async () => {
+      const { vaultHub } = ctx.contracts;
+      Tracing.enable();
+
+      const feesToClaim = await delegation.curatorUnclaimedFee();
+      // todo: feesToCalaim is 0 and method fails with ZeroArgument, how to fix it?
+      console.log(feesToClaim);
+      await expect(delegation.connect(curatorFeeClaimers).claimCuratorFee(stranger)).to.be.revertedWithCustomError(
+        vaultHub,
+        "NotConnectedToHub",
+      );
+      await connectToHub();
+
+      await expect(delegation.connect(curatorFeeClaimers).claimCuratorFee(stranger)).to.not.be.revertedWithCustomError(
+        vaultHub,
+        "NotConnectedToHub",
+      );
+      await disconnectFromHub();
+    });
+
+    it("to claim NO's fee", async () => {
+      const { vaultHub } = ctx.contracts;
+      Tracing.enable();
+
+      await expect(
+        delegation.connect(nodeOperatorManager).claimNodeOperatorFee(stranger),
+      ).to.be.revertedWithCustomError(vaultHub, "NotConnectedToHub");
+      await connectToHub();
+
+      await expect(
+        delegation.connect(nodeOperatorManager).claimNodeOperatorFee(stranger),
+      ).to.not.be.revertedWithCustomError(vaultHub, "NotConnectedToHub");
+      await disconnectFromHub();
+    });
+  });
+
+  it.skip("NO Manager can spawn a validator using ETH from the Vault ", () => {
+    // todo: what method to use?
+  });
+});
