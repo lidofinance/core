5de882fc96280547347bbd46e4ff95f4ce876e00
VP <vp@lido.fi>
2025-03-19 17:09:33 +0100

    feat: fix report params

--- test/integration/vaults/happy-path.integration.ts
+++ test/integration/vaults/happy-path.integration.ts
@@ -366,8 +366,7 @@ describe("Scenario: Staking Vaults Happy Path", () => {
     const params = {
       clDiff: elapsedProtocolReward,
       excludeVaultsBalances: true,
-      vaultValues: [vaultValue],
-      inOutDeltas: [VAULT_DEPOSIT],
+      vaultsTotalFees: vaultValue,
     } as OracleReportParams;
 
     const { reportTx } = (await report(ctx, params)) as {
