62007116541a9f9d2e4fd75c39675b82f63acdc1
failingtwice <azatsdev@gmail.com>
2025-04-17 14:59:18 +0500

    fix(WMP): update comment

--- contracts/0.8.4/WithdrawalsManagerProxy.sol
+++ contracts/0.8.4/WithdrawalsManagerProxy.sol
@@ -227,7 +227,7 @@ library Address {
  * instruction `delegatecall`. We refer to the second contract as the _implementation_ behind the proxy, and it has to
  * be specified by overriding the virtual {_implementation} function.
  *
- * Additionally, delegation to the implementation can be triggered manually through the {_fallback} function, or to a
+ * Additionally, dashboard to the implementation can be triggered manually through the {_fallback} function, or to a
  * different contract through the {_delegate} function.
  *
  * The success and return data of the delegated call will be returned back to the caller of the proxy.
