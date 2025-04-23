080cb2eba477b811eb082a78f9084cfb63e0f712
Evgeny Taktarov <je.mouth@gmail.com>
2025-04-23 16:39:32 +0700

    fix: integration tests

--- scripts/scratch/steps/0090-deploy-non-aragon-contracts.ts
+++ scripts/scratch/steps/0090-deploy-non-aragon-contracts.ts
@@ -215,7 +215,7 @@ export async function main() {
     proxyContractsOwner,
     deployer,
     [
-      state.chainSpec.forkVersion,
+      state.chainSpec.genesisForkVersion,
       pdgDeployParams.gIndex,
       pdgDeployParams.gIndexAfterChange,
       pdgDeployParams.changeSlot,
