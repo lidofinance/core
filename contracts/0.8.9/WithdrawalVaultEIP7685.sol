6a46d4662a00e063aa90d46575c6496cfa2e4efd
Maksim Kuraian <kuraian@proton.me>
2025-04-23 13:42:18 +0200

    feat: remove libs for withdrawal and consolidation requests

--- contracts/0.8.9/WithdrawalVaultEIP7685.sol
+++ contracts/0.8.9/WithdrawalVaultEIP7685.sol
@@ -7,25 +7,31 @@ pragma solidity 0.8.9;
 import {AccessControlEnumerable} from "./utils/access/AccessControlEnumerable.sol";
 import {PausableUntil} from "./utils/PausableUntil.sol";
 
-import {TriggerableWithdrawals} from "../common/lib/TriggerableWithdrawals.sol";
-import {Eip7251MaxEffectiveBalance} from "../common/lib/Eip7251MaxEffectiveBalance.sol";
-
 /**
  * @title A base contract for a withdrawal vault implementing EIP-7685: General Purpose Execution Layer Requests
  * @dev This contract enables validators to submit EIP-7002 withdrawal requests
  *      and manages the associated fees.
  */
 abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUntil {
+    address constant CONSOLIDATION_REQUEST = 0x0000BBdDc7CE488642fb579F8B00f3a590007251;
+    address constant WITHDRAWAL_REQUEST = 0x00000961Ef480Eb55e80D19ad83579A64c007002;
+
     bytes32 public constant PAUSE_ROLE = keccak256("PAUSE_ROLE");
     bytes32 public constant RESUME_ROLE = keccak256("RESUME_ROLE");
-    bytes32 public constant ADD_FULL_WITHDRAWAL_REQUEST_ROLE = keccak256("ADD_FULL_WITHDRAWAL_REQUEST_ROLE");
-    bytes32 public constant ADD_PARTIAL_WITHDRAWAL_REQUEST_ROLE = keccak256("ADD_PARTIAL_WITHDRAWAL_REQUEST_ROLE");
+    bytes32 public constant ADD_WITHDRAWAL_REQUEST_ROLE = keccak256("ADD_WITHDRAWAL_REQUEST_ROLE");
     bytes32 public constant ADD_CONSOLIDATION_REQUEST_ROLE = keccak256("ADD_CONSOLIDATION_REQUEST_ROLE");
 
     uint256 internal constant PUBLIC_KEY_LENGTH = 48;
 
-    error InsufficientFee(uint256 providedFee, uint256 requiredFee);
-    error ExcessFeeRefundFailed();
+    error ZeroArgument(string name);
+    error MalformedPubkeysArray();
+    error ArraysLengthMismatch(uint256 firstArrayLength, uint256 secondArrayLength);
+
+    error FeeReadFailed();
+    error FeeInvalidData();
+    error IncorrectFee(uint256 providedFee, uint256 requiredFee);
+
+    error RequestAdditionFailed(bytes callData);
 
     /// @dev Ensures the contract’s ETH balance is unchanged.
     modifier preservesEthBalance() {
@@ -69,66 +75,53 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
     }
 
     /**
-     * @dev Submits EIP-7002 full withdrawal requests for the specified public keys.
-     *      Each request instructs a validator to fully withdraw its stake and exit its duties as a validator.
-     *      Refunds any excess fee to the caller after deducting the total fees,
-     *      which are calculated based on the number of public keys and the current minimum fee per withdrawal request.
-     *
-     * @param pubkeys A tightly packed array of 48-byte public keys corresponding to validators requesting full withdrawals.
-     *                | ----- public key (48 bytes) ----- || ----- public key (48 bytes) ----- | ...
-     *
-     * @notice Reverts if:
-     *         - The caller does not have the `ADD_FULL_WITHDRAWAL_REQUEST_ROLE`.
-     *         - The provided public key array is empty.
-     *         - Validation of any of the provided public keys fails.
-     *         - The provided total withdrawal fee is insufficient to cover all requests.
-     *         - Refund of the excess fee fails.
-     */
-    function addFullWithdrawalRequests(
-        bytes calldata pubkeys
-    ) external payable onlyRole(ADD_FULL_WITHDRAWAL_REQUEST_ROLE) whenResumed preservesEthBalance {
-        uint256 feePerRequest = TriggerableWithdrawals.getWithdrawalRequestFee();
-        uint256 totalFee = _countPubkeys(pubkeys) * feePerRequest;
-
-        _requireSufficientFee(totalFee);
-
-        TriggerableWithdrawals.addFullWithdrawalRequests(pubkeys, feePerRequest);
-
-        _refundExcessFee(totalFee);
-    }
-
-    /**
-     * @dev Submits EIP-7002 partial withdrawal requests for the specified public keys with corresponding amounts.
-     *      Each request instructs a validator to withdraw a specified amount of ETH via their execution layer (0x01) withdrawal credentials.
-     *      Refunds any excess fee to the caller after deducting the total fees,
-     *      which are calculated based on the number of public keys and the current minimum fee per withdrawal request.
+     * @dev Submits EIP-7002 full or partial withdrawal requests for the specified public keys.
+     *      Each full withdrawal request instructs a validator to fully withdraw its stake and exit its duties as a validator.
+     *      Each partial withdrawal request instructs a validator to withdraw a specified amount of ETH.
      *
      * @param pubkeys A tightly packed array of 48-byte public keys corresponding to validators requesting partial withdrawals.
      *                | ----- public key (48 bytes) ----- || ----- public key (48 bytes) ----- | ...
      *
      * @param amounts An array of 8-byte unsigned integers representing the amounts to be withdrawn for each corresponding public key.
+     *                For full withdrawal requests, the amount should be set to 0.
+     *                For partial withdrawal requests, the amount should be greater than 0.
      *
      * @notice Reverts if:
-     *         - The caller does not have the `ADD_PARTIAL_WITHDRAWAL_REQUEST_ROLE`.
+     *         - The caller does not have the `ADD_WITHDRAWAL_REQUEST_ROLE`.
      *         - The provided public key array is empty.
+     *         - The provided public key array malformed.
      *         - The provided public key and amount arrays are not of equal length.
-     *         - Full withdrawal requested for any pubkeys (withdrawal amount = 0).
-     *         - Validation of any of the provided public keys fails.
-     *         - The provided total withdrawal fee is insufficient to cover all requests.
-     *         - Refund of the excess fee fails.
+     *         - The provided total withdrawal fee value is invalid.
      */
-    function addPartialWithdrawalRequests(
+    function addWithdrawalRequests(
         bytes calldata pubkeys,
         uint64[] calldata amounts
-    ) external payable onlyRole(ADD_PARTIAL_WITHDRAWAL_REQUEST_ROLE) whenResumed preservesEthBalance {
-        uint256 feePerRequest = TriggerableWithdrawals.getWithdrawalRequestFee();
-        uint256 totalFee = _countPubkeys(pubkeys) * feePerRequest;
+    ) external payable onlyRole(ADD_WITHDRAWAL_REQUEST_ROLE) whenResumed preservesEthBalance {
+        if (pubkeys.length == 0) revert ZeroArgument("pubkeys");
+        if (pubkeys.length % PUBLIC_KEY_LENGTH != 0) revert MalformedPubkeysArray();
 
-        _requireSufficientFee(totalFee);
+        uint256 requestsCount = _countPubkeys(pubkeys);
+        if (requestsCount != amounts.length) revert ArraysLengthMismatch(requestsCount, amounts.length);
 
-        TriggerableWithdrawals.addPartialWithdrawalRequests(pubkeys, amounts, feePerRequest);
+        uint256 feePerRequest = _getRequestFee(WITHDRAWAL_REQUEST);
+        uint256 totalFee = requestsCount * feePerRequest;
 
-        _refundExcessFee(totalFee);
+        _requireExactFee(totalFee);
+
+        bytes memory request = new bytes(56);
+        for (uint256 i = 0; i < requestsCount; i++) {
+            uint64 amount = amounts[i];
+            assembly {
+                calldatacopy(add(request, 32), add(pubkeys.offset, mul(i, PUBLIC_KEY_LENGTH)), PUBLIC_KEY_LENGTH)
+                mstore(add(request, 80), shl(192, amount))
+            }
+
+            (bool success, ) = WITHDRAWAL_REQUEST.call{value: feePerRequest}(request);
+
+            if (!success) {
+                revert RequestAdditionFailed(request);
+            }
+        }
     }
 
     /**
@@ -136,14 +129,11 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
      * @return The minimum fee required per withdrawal request.
      */
     function getWithdrawalRequestFee() external view returns (uint256) {
-        return TriggerableWithdrawals.getWithdrawalRequestFee();
+        return _getRequestFee(WITHDRAWAL_REQUEST);
     }
 
     /**
      * @dev Submits EIP-7251 consolidation requests for the specified public keys.
-     *      Each request consolidate validators.
-     *      Refunds any excess fee to the caller after deducting the total fees,
-     *      which are calculated based on the number of requests and the current minimum fee per withdrawal request.
      *
      * @param sourcePubkeys A tightly packed array of 48-byte source public keys corresponding to validators requesting consolidation.
      *                | ----- public key (48 bytes) ----- || ----- public key (48 bytes) ----- | ...
@@ -153,24 +143,38 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
      *
      * @notice Reverts if:
      *         - The caller does not have the `ADD_CONSOLIDATION_REQUEST_ROLE`.
-     *         - Validation of any of the provided public keys fails.
-     *         - The source and target public key arrays have different lengths.
-     *         - The provided public key arrays are empty.
-     *         - The provided total consolidation fee is insufficient to cover all requests.
-     *         - Refund of the excess fee fails.
+     *         - The provided public key array is empty.
+     *         - The provided public key array malformed.
+     *         - The provided source public key and target public key arrays are not of equal length.
+     *         - The provided total withdrawal fee value is invalid.
      */
     function addConsolidationRequests(
         bytes calldata sourcePubkeys,
         bytes calldata targetPubkeys
     ) external payable onlyRole(ADD_CONSOLIDATION_REQUEST_ROLE) whenResumed preservesEthBalance {
-        uint256 feePerRequest = Eip7251MaxEffectiveBalance.getConsolidationRequestFee();
-        uint256 totalFee = _countPubkeys(sourcePubkeys) * feePerRequest;
+        if (sourcePubkeys.length == 0) revert ZeroArgument("sourcePubkeys");
+        if (sourcePubkeys.length % PUBLIC_KEY_LENGTH != 0) revert MalformedPubkeysArray();
+        if (sourcePubkeys.length != targetPubkeys.length)
+            revert ArraysLengthMismatch(sourcePubkeys.length, sourcePubkeys.length);
 
-        _requireSufficientFee(totalFee);
+        uint256 requestsCount = _countPubkeys(sourcePubkeys);
+        uint256 feePerRequest = _getRequestFee(CONSOLIDATION_REQUEST);
 
-        Eip7251MaxEffectiveBalance.addConsolidationRequests(sourcePubkeys, targetPubkeys, feePerRequest);
+        _requireExactFee(requestsCount * feePerRequest);
 
-        _refundExcessFee(totalFee);
+        bytes memory request = new bytes(96);
+        for (uint256 i = 0; i < requestsCount; i++) {
+            assembly {
+                calldatacopy(add(request, 32), add(sourcePubkeys.offset, mul(i, PUBLIC_KEY_LENGTH)), PUBLIC_KEY_LENGTH)
+                calldatacopy(add(request, 80), add(targetPubkeys.offset, mul(i, PUBLIC_KEY_LENGTH)), PUBLIC_KEY_LENGTH)
+            }
+
+            (bool success, ) = CONSOLIDATION_REQUEST.call{value: feePerRequest}(request);
+
+            if (!success) {
+                revert RequestAdditionFailed(request);
+            }
+        }
     }
 
     /**
@@ -178,27 +182,30 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
      * @return The minimum fee required per consolidation request.
      */
     function getConsolidationRequestFee() external view returns (uint256) {
-        return Eip7251MaxEffectiveBalance.getConsolidationRequestFee();
+        return _getRequestFee(CONSOLIDATION_REQUEST);
+    }
+
+    function _getRequestFee(address requestedContract) internal view returns (uint256) {
+        (bool success, bytes memory feeData) = requestedContract.staticcall("");
+
+        if (!success) {
+            revert FeeReadFailed();
+        }
+
+        if (feeData.length != 32) {
+            revert FeeInvalidData();
+        }
+
+        return abi.decode(feeData, (uint256));
     }
 
     function _countPubkeys(bytes calldata pubkeys) internal pure returns (uint256) {
         return (pubkeys.length / PUBLIC_KEY_LENGTH);
     }
 
-    function _requireSufficientFee(uint256 requiredFee) internal view {
-        if (requiredFee > msg.value) {
-            revert InsufficientFee(msg.value, requiredFee);
-        }
-    }
-
-    function _refundExcessFee(uint256 fee) internal {
-        uint256 refund = msg.value - fee;
-        if (refund > 0) {
-            (bool success, ) = msg.sender.call{value: refund}("");
-
-            if (!success) {
-                revert ExcessFeeRefundFailed();
-            }
+    function _requireExactFee(uint256 requiredFee) internal view {
+        if (requiredFee != msg.value) {
+            revert IncorrectFee(msg.value, requiredFee);
         }
     }
 }
