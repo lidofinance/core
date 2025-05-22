159bb13a1ed7039b6157ca138b492c23657c2e54
Artyom Veremeenko <arwer13@gmail.com>
2025-05-22 10:47:15 +0300

    feat(upgrade): remove immutable roles from upgrade template

--- contracts/0.8.25/upgrade/V3Template.sol
+++ contracts/0.8.25/upgrade/V3Template.sol
@@ -77,47 +77,14 @@ contract V3Template is V3Addresses {
     event UpgradeFinished();
 
     //
-    // Constants and immutables
+    // -------- Constants --------
     //
 
     uint256 internal constant ACCOUNTING_ORACLE_CONSENSUS_VERSION = 4;
     uint256 internal constant EXPECTED_FINAL_LIDO_VERSION = 3;
     uint256 internal constant EXPECTED_FINAL_ACCOUNTING_ORACLE_VERSION = 3;
 
-    //
-    // -------- Roles --------
-    //
-    // NB: Storing the roles as immutables to save gas
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
-    // Burner
-    bytes32 public immutable REQUEST_BURN_SHARES_ROLE;
-    // PauseUntilWithRoles
-    bytes32 public immutable RESUME_ROLE;
-    bytes32 public immutable PAUSE_ROLE;
-    // OracleReportSanityChecker
-    bytes32 public immutable ALL_LIMITS_MANAGER_ROLE;
-    bytes32 public immutable EXITED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE;
-    bytes32 public immutable APPEARED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE;
-    bytes32 public immutable ANNUAL_BALANCE_INCREASE_LIMIT_MANAGER_ROLE;
-    bytes32 public immutable SHARE_RATE_DEVIATION_LIMIT_MANAGER_ROLE;
-    bytes32 public immutable MAX_VALIDATOR_EXIT_REQUESTS_PER_REPORT_ROLE;
-    bytes32 public immutable MAX_ITEMS_PER_EXTRA_DATA_TRANSACTION_ROLE;
-    bytes32 public immutable MAX_NODE_OPERATORS_PER_EXTRA_DATA_ITEM_ROLE;
-    bytes32 public immutable REQUEST_TIMESTAMP_MARGIN_MANAGER_ROLE;
-    bytes32 public immutable MAX_POSITIVE_TOKEN_REBASE_MANAGER_ROLE;
-    bytes32 public immutable SECOND_OPINION_MANAGER_ROLE;
-    bytes32 public immutable INITIAL_SLASHING_AND_PENALTIES_MANAGER_ROLE;
-    // StakingRouter
-    bytes32 public immutable REPORT_REWARDS_MINTED_ROLE;
-    // VaultHub
-    bytes32 public immutable VAULT_MASTER_ROLE;
-    bytes32 public immutable VAULT_REGISTRY_ROLE;
-    // OperatorGrid
-    bytes32 public immutable REGISTRY_ROLE;
-
-    //
-    // -------- Constants --------
-    //
 
     // Timestamp since startUpgrade() and finishUpgrade() revert with Expired()
     // This behavior is introduced to disarm the template if the upgrade voting creation or enactment didn't
@@ -142,34 +109,6 @@ contract V3Template is V3Addresses {
 
     /// @param _params Params required to initialize the addresses contract
     constructor(V3AddressesParams memory _params) V3Addresses(_params) {
-        //
-        // -------- Initialize Roles --------
-        //
-        // Initialize Burner roles
-        REQUEST_BURN_SHARES_ROLE = IBurner(BURNER).REQUEST_BURN_SHARES_ROLE();
-        // Initialize PauseUntilWithRoles roles
-        RESUME_ROLE = VaultHub(VAULT_HUB).RESUME_ROLE();
-        PAUSE_ROLE = VaultHub(VAULT_HUB).PAUSE_ROLE();
-        // Initialize OracleReportSanityChecker roles
-        ALL_LIMITS_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).ALL_LIMITS_MANAGER_ROLE();
-        EXITED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).EXITED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE();
-        APPEARED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).APPEARED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE();
-        ANNUAL_BALANCE_INCREASE_LIMIT_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).ANNUAL_BALANCE_INCREASE_LIMIT_MANAGER_ROLE();
-        SHARE_RATE_DEVIATION_LIMIT_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).SHARE_RATE_DEVIATION_LIMIT_MANAGER_ROLE();
-        MAX_VALIDATOR_EXIT_REQUESTS_PER_REPORT_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).MAX_VALIDATOR_EXIT_REQUESTS_PER_REPORT_ROLE();
-        MAX_ITEMS_PER_EXTRA_DATA_TRANSACTION_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).MAX_ITEMS_PER_EXTRA_DATA_TRANSACTION_ROLE();
-        MAX_NODE_OPERATORS_PER_EXTRA_DATA_ITEM_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).MAX_NODE_OPERATORS_PER_EXTRA_DATA_ITEM_ROLE();
-        REQUEST_TIMESTAMP_MARGIN_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).REQUEST_TIMESTAMP_MARGIN_MANAGER_ROLE();
-        MAX_POSITIVE_TOKEN_REBASE_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).MAX_POSITIVE_TOKEN_REBASE_MANAGER_ROLE();
-        SECOND_OPINION_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).SECOND_OPINION_MANAGER_ROLE();
-        INITIAL_SLASHING_AND_PENALTIES_MANAGER_ROLE = IOracleReportSanityChecker(ORACLE_REPORT_SANITY_CHECKER).INITIAL_SLASHING_AND_PENALTIES_MANAGER_ROLE();
-        // Initialize StakingRouter roles
-        REPORT_REWARDS_MINTED_ROLE = IStakingRouter(STAKING_ROUTER).REPORT_REWARDS_MINTED_ROLE();
-        // Initialize VaultHub roles
-        VAULT_MASTER_ROLE = VaultHub(VAULT_HUB).VAULT_MASTER_ROLE();
-        VAULT_REGISTRY_ROLE = VaultHub(VAULT_HUB).VAULT_REGISTRY_ROLE();
-        // Initialize OperatorGrid roles
-        REGISTRY_ROLE = OperatorGrid(OPERATOR_GRID).REGISTRY_ROLE();
     }
 
     /// @notice Must be called after LidoLocator is upgraded
@@ -265,6 +204,7 @@ contract V3Template is V3Addresses {
         address agent = AGENT;
 
         // Burner
+        bytes32 requestBurnSharesRole = IBurner(BURNER).REQUEST_BURN_SHARES_ROLE();
         _assertSingleOZRoleHolder(IBurner(BURNER), DEFAULT_ADMIN_ROLE, agent);
         {
             address[] memory holders = new address[](4);
@@ -272,14 +212,14 @@ contract V3Template is V3Addresses {
             holders[1] = NODE_OPERATORS_REGISTRY;
             holders[2] = SIMPLE_DVT;
             holders[3] = ACCOUNTING;
-            _assertOZRoleHolders(IBurner(BURNER), REQUEST_BURN_SHARES_ROLE, holders);
+            _assertOZRoleHolders(IBurner(BURNER), requestBurnSharesRole, holders);
         }
-        _assertZeroOZRoleHolders(IBurner(OLD_BURNER), REQUEST_BURN_SHARES_ROLE);
+        _assertZeroOZRoleHolders(IBurner(OLD_BURNER), requestBurnSharesRole);
 
         // VaultHub
         _assertSingleOZRoleHolder(IAccessControlEnumerable(VAULT_HUB), DEFAULT_ADMIN_ROLE, agent);
-        _assertSingleOZRoleHolder(IAccessControlEnumerable(VAULT_HUB), VAULT_MASTER_ROLE, agent);
-        _assertZeroOZRoleHolders(IAccessControlEnumerable(VAULT_HUB), VAULT_REGISTRY_ROLE);
+        _assertSingleOZRoleHolder(IAccessControlEnumerable(VAULT_HUB), VaultHub(VAULT_HUB).VAULT_MASTER_ROLE(), agent);
+        _assertZeroOZRoleHolders(IAccessControlEnumerable(VAULT_HUB), VaultHub(VAULT_HUB).VAULT_REGISTRY_ROLE());
         _assertProxyAdmin(IOssifiableProxy(VAULT_HUB), agent);
         // TODO: add PausableUntilWithRoles checks when gate seal is added
 
@@ -291,18 +231,18 @@ contract V3Template is V3Addresses {
         _assertSingleOZRoleHolder(checker, DEFAULT_ADMIN_ROLE, AGENT);
 
         bytes32[12] memory roles = [
-            ALL_LIMITS_MANAGER_ROLE,
-            EXITED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE,
-            APPEARED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE,
-            ANNUAL_BALANCE_INCREASE_LIMIT_MANAGER_ROLE,
-            SHARE_RATE_DEVIATION_LIMIT_MANAGER_ROLE,
-            MAX_VALIDATOR_EXIT_REQUESTS_PER_REPORT_ROLE,
-            MAX_ITEMS_PER_EXTRA_DATA_TRANSACTION_ROLE,
-            MAX_NODE_OPERATORS_PER_EXTRA_DATA_ITEM_ROLE,
-            REQUEST_TIMESTAMP_MARGIN_MANAGER_ROLE,
-            MAX_POSITIVE_TOKEN_REBASE_MANAGER_ROLE,
-            SECOND_OPINION_MANAGER_ROLE,
-            INITIAL_SLASHING_AND_PENALTIES_MANAGER_ROLE
+            checker.ALL_LIMITS_MANAGER_ROLE(),
+            checker.EXITED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE(),
+            checker.APPEARED_VALIDATORS_PER_DAY_LIMIT_MANAGER_ROLE(),
+            checker.ANNUAL_BALANCE_INCREASE_LIMIT_MANAGER_ROLE(),
+            checker.SHARE_RATE_DEVIATION_LIMIT_MANAGER_ROLE(),
+            checker.MAX_VALIDATOR_EXIT_REQUESTS_PER_REPORT_ROLE(),
+            checker.MAX_ITEMS_PER_EXTRA_DATA_TRANSACTION_ROLE(),
+            checker.MAX_NODE_OPERATORS_PER_EXTRA_DATA_ITEM_ROLE(),
+            checker.REQUEST_TIMESTAMP_MARGIN_MANAGER_ROLE(),
+            checker.MAX_POSITIVE_TOKEN_REBASE_MANAGER_ROLE(),
+            checker.SECOND_OPINION_MANAGER_ROLE(),
+            checker.INITIAL_SLASHING_AND_PENALTIES_MANAGER_ROLE()
         ];
         for (uint256 i = 0; i < roles.length; ++i) {
             _assertZeroOZRoleHolders(checker, roles[i]);
@@ -319,13 +259,13 @@ contract V3Template is V3Addresses {
             address[] memory holders = new address[](2);
             holders[0] = LIDO;
             holders[1] = ACCOUNTING;
-            _assertOZRoleHolders(IAccessControlEnumerable(STAKING_ROUTER), REPORT_REWARDS_MINTED_ROLE, holders);
+            _assertOZRoleHolders(IAccessControlEnumerable(STAKING_ROUTER), IStakingRouter(STAKING_ROUTER).REPORT_REWARDS_MINTED_ROLE(), holders);
         }
 
         // OperatorGrid
         _assertProxyAdmin(IOssifiableProxy(OPERATOR_GRID), agent);
         _assertSingleOZRoleHolder(IAccessControlEnumerable(OPERATOR_GRID), DEFAULT_ADMIN_ROLE, agent);
-        _assertSingleOZRoleHolder(IAccessControlEnumerable(OPERATOR_GRID), REGISTRY_ROLE, agent);
+        _assertSingleOZRoleHolder(IAccessControlEnumerable(OPERATOR_GRID), OperatorGrid(OPERATOR_GRID).REGISTRY_ROLE(), agent);
     }
 
     function _checkBurnerMigratedCorrectly() internal view {
