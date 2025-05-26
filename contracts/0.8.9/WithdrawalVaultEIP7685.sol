3b27659b7f286596ad77cd1fa0931d489c8e45a2
KRogLA <i@krogla.me>
2025-05-27 01:28:10 +0200

    refactor: withdrawal vault + tw gateway

--- contracts/0.8.9/WithdrawalVaultEIP7685.sol
+++ contracts/0.8.9/WithdrawalVaultEIP7685.sol
@@ -4,74 +4,22 @@
 /* See contracts/COMPILERS.md */
 pragma solidity 0.8.9;
 
-import {AccessControlEnumerable} from "./utils/access/AccessControlEnumerable.sol";
-import {PausableUntil} from "./utils/PausableUntil.sol";
-
 /**
  * @title A base contract for a withdrawal vault implementing EIP-7685: General Purpose Execution Layer Requests
  * @dev This contract enables validators to submit EIP-7002 withdrawal requests.
  */
-abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUntil {
+abstract contract WithdrawalVaultEIP7685 {
     address constant WITHDRAWAL_REQUEST = 0x00000961Ef480Eb55e80D19ad83579A64c007002;
-
-    bytes32 public constant PAUSE_ROLE = keccak256("PAUSE_ROLE");
-    bytes32 public constant RESUME_ROLE = keccak256("RESUME_ROLE");
-    bytes32 public constant ADD_WITHDRAWAL_REQUEST_ROLE = keccak256("ADD_WITHDRAWAL_REQUEST_ROLE");
-
-    uint256 internal constant PUBLIC_KEY_LENGTH = 48;
 
     event WithdrawalRequestAdded(bytes request);
 
     error ZeroArgument(string name);
-    error MalformedPubkeysArray();
     error ArraysLengthMismatch(uint256 firstArrayLength, uint256 secondArrayLength);
-
     error FeeReadFailed();
     error FeeInvalidData();
     error IncorrectFee(uint256 providedFee, uint256 requiredFee);
-
     error RequestAdditionFailed(bytes callData);
 
-    /// @dev Ensures the contract’s ETH balance is unchanged.
-    modifier preservesEthBalance() {
-        uint256 balanceBeforeCall = address(this).balance - msg.value;
-        _;
-        assert(address(this).balance == balanceBeforeCall);
-    }
-
-    /**
-     * @dev Resumes the general purpose execution layer requests.
-     * @notice Reverts if:
-     *         - The contract is not paused.
-     *         - The sender does not have the `RESUME_ROLE`.
-     */
-    function resume() external onlyRole(RESUME_ROLE) {
-        _resume();
-    }
-
-    /**
-     * @notice Pauses the general purpose execution layer requests placement for a specified duration.
-     * @param _duration The pause duration in seconds (use `PAUSE_INFINITELY` for unlimited).
-     * @dev Reverts if:
-     *         - The contract is already paused.
-     *         - The sender does not have the `PAUSE_ROLE`.
-     *         - A zero duration is passed.
-     */
-    function pauseFor(uint256 _duration) external onlyRole(PAUSE_ROLE) {
-        _pauseFor(_duration);
-    }
-
-    /**
-     * @notice Pauses the general purpose execution layer requests placement until a specified timestamp.
-     * @param _pauseUntilInclusive The last second to pause until (inclusive).
-     * @dev Reverts if:
-     *         - The timestamp is in the past.
-     *         - The sender does not have the `PAUSE_ROLE`.
-     *         - The contract is already paused.
-     */
-    function pauseUntil(uint256 _pauseUntilInclusive) external onlyRole(PAUSE_ROLE) {
-        _pauseUntil(_pauseUntilInclusive);
-    }
 
     /**
      * @dev Submits EIP-7002 full or partial withdrawal requests for the specified public keys.
@@ -92,38 +40,16 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
      *         - The provided public key and amount arrays are not of equal length.
      *         - The provided total withdrawal fee value is invalid.
      */
-    function addWithdrawalRequests(
-        bytes calldata pubkeys,
-        uint64[] calldata amounts
-    ) external payable onlyRole(ADD_WITHDRAWAL_REQUEST_ROLE) whenResumed preservesEthBalance {
-        if (pubkeys.length == 0) revert ZeroArgument("pubkeys");
-        if (pubkeys.length % PUBLIC_KEY_LENGTH != 0) revert MalformedPubkeysArray();
-
-        uint256 requestsCount = pubkeys.length / PUBLIC_KEY_LENGTH;
+    function _addWithdrawalRequests(bytes[] calldata pubkeys, uint64[] calldata amounts) internal {
+        uint256 requestsCount = pubkeys.length;
+        if (requestsCount == 0) revert ZeroArgument("pubkeys");
         if (requestsCount != amounts.length) revert ArraysLengthMismatch(requestsCount, amounts.length);
 
-        uint256 feePerRequest = _getRequestFee(WITHDRAWAL_REQUEST);
-        uint256 totalFee = requestsCount * feePerRequest;
+        uint256 fee = getWithdrawalRequestFee();
+        _checkFee(requestsCount * fee);
 
-        if (totalFee != msg.value) {
-            revert IncorrectFee(msg.value, totalFee);
-        }
-
-        bytes memory request = new bytes(56);
-        for (uint256 i = 0; i < requestsCount; i++) {
-            uint64 amount = amounts[i];
-            assembly {
-                calldatacopy(add(request, 32), add(pubkeys.offset, mul(i, PUBLIC_KEY_LENGTH)), PUBLIC_KEY_LENGTH)
-                mstore(add(request, 80), shl(192, amount))
-            }
-
-            (bool success, ) = WITHDRAWAL_REQUEST.call{value: feePerRequest}(request);
-
-            if (!success) {
-                revert RequestAdditionFailed(request);
-            }
-
-            emit WithdrawalRequestAdded(request);
+        for (uint256 i = 0; i < requestsCount; ++i) {
+            _callAddWithdrawalRequest(fee, abi.encodePacked(pubkeys[i], amounts[i]));
         }
     }
 
@@ -131,7 +57,7 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
      * @dev Retrieves the current EIP-7002 withdrawal fee.
      * @return The minimum fee required per withdrawal request.
      */
-    function getWithdrawalRequestFee() external view returns (uint256) {
+    function getWithdrawalRequestFee() public view returns (uint256) {
         return _getRequestFee(WITHDRAWAL_REQUEST);
     }
 
@@ -147,5 +73,20 @@ abstract contract WithdrawalVaultEIP7685 is AccessControlEnumerable, PausableUnt
         }
 
         return abi.decode(feeData, (uint256));
+    }
+
+    function _callAddWithdrawalRequest(uint256 fee, bytes memory request) internal {
+        (bool success,) = WITHDRAWAL_REQUEST.call{value: fee}(request);
+        if (!success) {
+            revert RequestAdditionFailed(request);
+        }
+
+        emit WithdrawalRequestAdded(request);
+    }
+
+    function _checkFee(uint256 fee) internal view {
+        if (msg.value != fee) {
+            revert IncorrectFee(msg.value, fee);
+        }
     }
 }
