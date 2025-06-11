fe7df7014670174d260c72d6cad2c170690b4d11
Maksim Kuraian <kuraian@proton.me>
2025-06-11 12:19:29 +0200

    Update contracts/common/lib/BeaconTypes.sol
    Co-authored-by: Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>

--- contracts/common/lib/BeaconTypes.sol
+++ contracts/common/lib/BeaconTypes.sol
@@ -1,6 +1,8 @@
 // SPDX-FileCopyrightText: 2025 Lido <info@lido.fi>
 // SPDX-License-Identifier: GPL-3.0
 
+// See contracts/COMPILERS.md
+// solhint-disable-next-line lido/fixed-compiler-version
 pragma solidity ^0.8.25;
 
 struct Validator {
