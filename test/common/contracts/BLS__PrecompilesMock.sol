26d256c6baa37be1b99c9b74a24e5ec50c611aca
Artyom Veremeenko <arwer13@gmail.com>
2025-06-13 12:36:26 +0300

    fix: foundry SSZ BLS tests

--- test/common/contracts/BLS__PrecompilesMock.sol
+++ test/common/contracts/BLS__PrecompilesMock.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 // for testing purposes only
 
-pragma solidity 0.8.25;
+// See contracts/COMPILERS.md
+// solhint-disable-next-line lido/fixed-compiler-version
+pragma solidity ^0.8.25;
 
 contract BLSG2ADD__Mock {
     fallback(bytes calldata) external returns (bytes memory) {
