09003797cd034375bc26b5e8ba252c93377d0f6d
Eddort <webisforce@gmail.com>
2025-06-15 11:41:34 +0200

    fix: contracts verification

--- scripts/deploy-tw.sh
+++ scripts/deploy-tw.sh
@@ -7,7 +7,7 @@ export RPC_URL=${RPC_URL:="http://127.0.0.1:8545"}  # if defined use the value s
 # export WITHDRAWAL_QUEUE_BASE_URI="<< SET IF REQUIED >>"
 # export DSM_PREDEFINED_ADDRESS="<< SET IF REQUIED >>"
 
-export DEPLOYER=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266  # first acc of default mnemonic "test test ..."
+export DEPLOYER=${DEPLOYER:="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}  # first acc of default mnemonic "test test ..."
 export GAS_PRIORITY_FEE=1
 export GAS_MAX_FEE=100
 
--- tasks/verify-contracts.ts
+++ tasks/verify-contracts.ts
@@ -99,6 +99,8 @@ async function verifyContract(contract: DeployedContract, hre: HardhatRuntimeEnv
 function getDeployedContract(contract: Contract): DeployedContract[] {
   if ("proxy" in contract && "implementation" in contract) {
     return [contract.proxy, contract.implementation];
+  } else if ("implementation" in contract) {
+    return [contract.implementation as DeployedContract];
   } else if ("contract" in contract && "address" in contract && "constructorArgs" in contract) {
     return [contract];
   }
