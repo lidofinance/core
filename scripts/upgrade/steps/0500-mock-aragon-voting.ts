6087ba40b0a46d7630b1c25115306e01dc5b2f99
chasingrainbows <chasing.rainbows@lido.fi>
2025-07-10 18:50:09 +0300

    feat(orc-420): fix tests for DG

--- scripts/upgrade/steps/0500-mock-aragon-voting.ts
+++ scripts/upgrade/steps/0500-mock-aragon-voting.ts
@@ -1,32 +1,11 @@
-import { TokenManager, TWVoteScript, Voting } from "typechain-types";
+import { mockDGAragonVoting } from "scripts/utils/upgrade";
 
-import { advanceChainTime, ether, log } from "lib";
-import { impersonate } from "lib/account";
-import { loadContract } from "lib/contract";
 import { readNetworkState, Sk } from "lib/state-file";
 
-export async function main(): Promise<void> {
+export async function main(): Promise<ReturnType<typeof mockDGAragonVoting>> {
   const state = readNetworkState();
-  log("Starting mock Aragon voting...");
-  const agentAddress = state[Sk.appAgent].proxy.address;
-  const votingAddress = state[Sk.appVoting].proxy.address;
-  const tokenManagerAddress = state[Sk.appTokenManager].proxy.address;
-
-  const deployer = await impersonate(agentAddress, ether("100"));
-
-  const voteScript = await loadContract<TWVoteScript>("TWVoteScript", state[Sk.TWVoteScript].address);
-  const tokenManager = await loadContract<TokenManager>("TokenManager", tokenManagerAddress);
-  const voting = await loadContract<Voting>("Voting", votingAddress);
-
-  const voteId = await voting.votesLength();
-  console.log(await voteScript.getDebugParams())
-  const newVoteBytecode = await voteScript.getNewVoteCallBytecode("TW Lido Upgrade description placeholder");
-  await tokenManager.connect(deployer).forward(newVoteBytecode);
-  if (!(await voteScript.isValidVoteScript(voteId))) throw new Error("Vote script is not valid");
-  await voting.connect(deployer).vote(voteId, true, false);
-  await advanceChainTime(await voting.voteTime());
-  const executeTx = await voting.executeVote(voteId);
-
-  const executeReceipt = await executeTx.wait();
-  log.success("Voting executed: gas used", executeReceipt!.gasUsed);
+  const voteScriptAddress = state[Sk.TWVoteScript].address;
+  const votingDescription = "TW Lido Upgrade description placeholder";
+  const proposalMetadata = "TW Lido Upgrade proposal metadata placeholder";
+  return mockDGAragonVoting(voteScriptAddress, votingDescription, proposalMetadata, state);
 }
