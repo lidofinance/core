77828f89bb85ed74e3e5bb8eabebb5f5644be01f
Artyom Veremeenko <arwer13@gmail.com>
2025-07-14 21:27:51 +0300

    chore: nitpicking in BaseOracle and InterfacesMatcher.0.8.9

--- test/common/contracts/InterfacesMatcher.0.8.9.sol
+++ test/common/contracts/InterfacesMatcher.0.8.9.sol
@@ -5,9 +5,10 @@ pragma solidity 0.8.9;
 import {IHashConsensus} from "contracts/common/interfaces/IHashConsensus.sol";
 import {HashConsensus} from "contracts/0.8.9/oracle/HashConsensus.sol";
 
-// This contract is used to match the interfaces and the contracts
-// without need to modify the contract source - to avoid discrepancy between the contract
-// source in the repository and the contract source code deployed to sources explorer.
+// This contract is utilized to align interfaces with contracts
+// without requiring modifications to the contract source. This approach
+// prevents discrepancies between the repository's contract source and the
+// contract source code deployed to a source explorer (e.g., Etherscan).
 abstract contract InterfacesMatcher_0_8_9 {
     constructor() {
         IHashConsensus(address(new HashConsensus(0, 0, 0, 0, 0, address(0), address(0))));
