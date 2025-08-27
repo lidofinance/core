061e27d8f6c75026ba06941128759ceffb0123dd
hweawer <alexandergusakovsky@gmail.com>
2025-08-27 14:31:07 +0200

    feat(tw-deploy): gateseal state and network state var

--- scripts/tw-deploy.sh
+++ scripts/tw-deploy.sh
@@ -11,7 +11,7 @@ export DEPLOYER=${DEPLOYER:="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}  # fir
 export GAS_PRIORITY_FEE=1
 export GAS_MAX_FEE=100
 
-export NETWORK_STATE_FILE="deployed-hoodi.json"
+export NETWORK_STATE_FILE=${NETWORK_STATE_FILE:="deployed-hoodi.json"}
 # export NETWORK_STATE_DEFAULTS_FILE="scripts/scratch/deployed-testnet-defaults.json"
 
 
