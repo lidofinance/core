d56ff5d230ec56503907275a56f077699ab509de
Maksim Kuraian <kuraian@proton.me>
2025-08-28 14:57:00 +0200

    feat: add consolidation request tests

--- test/0.8.9/withdrawalVault/withdrawalVault.test.ts
+++ test/0.8.9/withdrawalVault/withdrawalVault.test.ts
@@ -7,13 +7,21 @@ import { setBalance } from "@nomicfoundation/hardhat-network-helpers";
 
 import {
   EIP7002WithdrawalRequest__Mock,
+  EIP7251ConsolidationRequest__Mock,
   ERC20__Harness,
   ERC721__Harness,
   Lido__MockForWithdrawalVault,
   WithdrawalVault__Harness,
 } from "typechain-types";
 
-import { EIP7002_ADDRESS, EIP7002_MIN_WITHDRAWAL_REQUEST_FEE, MAX_UINT256, proxify } from "lib";
+import {
+  EIP7002_ADDRESS,
+  EIP7002_MIN_WITHDRAWAL_REQUEST_FEE,
+  EIP7251_ADDRESS,
+  EIP7251_MIN_CONSOLIDATION_FEE,
+  MAX_UINT256,
+  proxify,
+} from "lib";
 
 import { Snapshot } from "test/suite";
 
@@ -23,7 +31,13 @@ import {
   findEIP7002MockEvents,
   testEIP7002Mock,
 } from "./eip7002Mock";
-import { generateWithdrawalRequestPayload } from "./utils";
+import {
+  deployEIP7251ConsolidationRequestContractMock,
+  encodeEIP7251Payload,
+  findEIP7251MockEvents,
+  testEIP7251Mock,
+} from "./eip7251Mock";
+import { generateConsolidationRequestPayload, generateWithdrawalRequestPayload } from "./utils";
 
 const PETRIFIED_VERSION = MAX_UINT256;
 
@@ -38,6 +52,7 @@ describe("WithdrawalVault.sol", () => {
   let originalState: string;
 
   let withdrawalsPredeployed: EIP7002WithdrawalRequest__Mock;
+  let consolidationPredeployed: EIP7251ConsolidationRequest__Mock;
   let lido: Lido__MockForWithdrawalVault;
   let lidoAddress: string;
 
@@ -53,6 +68,10 @@ describe("WithdrawalVault.sol", () => {
     withdrawalsPredeployed = await deployEIP7002WithdrawalRequestContractMock(EIP7002_MIN_WITHDRAWAL_REQUEST_FEE);
 
     expect(await withdrawalsPredeployed.getAddress()).to.equal(EIP7002_ADDRESS);
+
+    consolidationPredeployed = await deployEIP7251ConsolidationRequestContractMock(EIP7251_MIN_CONSOLIDATION_FEE);
+
+    expect(await consolidationPredeployed.getAddress()).to.equal(EIP7251_ADDRESS);
 
     lido = await ethers.deployContract("Lido__MockForWithdrawalVault");
     lidoAddress = await lido.getAddress();
@@ -613,6 +632,351 @@ describe("WithdrawalVault.sol", () => {
           vebInitialBalance -
             expectedTotalWithdrawalFee -
             receiptPartialWithdrawal.gasUsed * receiptPartialWithdrawal.gasPrice,
+        );
+      });
+    });
+  });
+
+  context("get consolidation request fee", () => {
+    it("Should get fee from the EIP-7251 contract", async function () {
+      await consolidationPredeployed.mock__setFee(333n);
+      expect(
+        (await vault.getConsolidationRequestFee()) == 333n,
+        "consolidation request should use fee from the EIP-7251 contract",
+      );
+    });
+
+    it("Should revert if fee read fails", async function () {
+      await consolidationPredeployed.mock__setFailOnGetFee(true);
+      await expect(vault.getConsolidationRequestFee()).to.be.revertedWithCustomError(vault, "FeeReadFailed");
+    });
+
+    ["0x", "0x01", "0x" + "0".repeat(61) + "1", "0x" + "0".repeat(65) + "1"].forEach((unexpectedFee) => {
+      it(`Should revert if unexpected fee value ${unexpectedFee} is returned`, async function () {
+        await consolidationPredeployed.mock__setFeeRaw(unexpectedFee);
+        await expect(vault.getConsolidationRequestFee()).to.be.revertedWithCustomError(vault, "FeeInvalidData");
+      });
+    });
+  });
+
+  async function getConsolidationFee(): Promise<bigint> {
+    const fee = await vault.getConsolidationRequestFee();
+
+    return ethers.parseUnits(fee.toString(), "wei");
+  }
+
+  async function getConsolidationPredeployedContractBalance(): Promise<bigint> {
+    const contractAddress = await consolidationPredeployed.getAddress();
+    return await ethers.provider.getBalance(contractAddress);
+  }
+
+  context("add consolidation requests", () => {
+    beforeEach(async () => {
+      await vault.initialize();
+    });
+
+    it("Should revert if the caller is not Consolidation Gateway", async () => {
+      await expect(
+        vault.connect(stranger).addConsolidationRequests(["0x1234"], ["0x5678"]),
+      ).to.be.revertedWithCustomError(vault, "NotConsolidationGateway");
+    });
+
+    it("Should revert if empty arrays are provided", async function () {
+      await expect(vault.connect(consolidationGateway).addConsolidationRequests([], [], { value: 1n }))
+        .to.be.revertedWithCustomError(vault, "ZeroArgument")
+        .withArgs("sourcePubkeys");
+    });
+
+    it("Should revert if array lengths do not match", async function () {
+      const requestCount = 2;
+      const { sourcePubkeysHexArray } = generateConsolidationRequestPayload(requestCount);
+      const { targetPubkeysHexArray } = generateConsolidationRequestPayload(1); // Only one target pubkey
+
+      const totalConsolidationFee = (await getConsolidationFee()) * BigInt(requestCount);
+
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: totalConsolidationFee }),
+      )
+        .to.be.revertedWithCustomError(vault, "ArraysLengthMismatch")
+        .withArgs(requestCount, targetPubkeysHexArray.length);
+
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests(sourcePubkeysHexArray, [], { value: totalConsolidationFee }),
+      )
+        .to.be.revertedWithCustomError(vault, "ArraysLengthMismatch")
+        .withArgs(requestCount, 0);
+    });
+
+    it("Should revert if not enough fee is sent", async function () {
+      const { sourcePubkeysHexArray, targetPubkeysHexArray } = generateConsolidationRequestPayload(1);
+
+      await consolidationPredeployed.mock__setFee(3n); // Set fee to 3 gwei
+
+      // 1. Should revert if no fee is sent
+      await expect(
+        vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray),
+      )
+        .to.be.revertedWithCustomError(vault, "IncorrectFee")
+        .withArgs(3n, 0);
+
+      // 2. Should revert if fee is less than required
+      const insufficientFee = 2n;
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: insufficientFee }),
+      )
+        .to.be.revertedWithCustomError(vault, "IncorrectFee")
+        .withArgs(3n, 2n);
+    });
+
+    it("Should revert if source pubkey is not 48 bytes", async function () {
+      // Invalid source pubkey (only 2 bytes)
+      const invalidSourcePubkey = "0x1234";
+      const validTargetPubkey = "0x" + "5".repeat(96); // 48 bytes
+
+      const fee = await getConsolidationFee();
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests([invalidSourcePubkey], [validTargetPubkey], { value: fee }),
+      )
+        .to.be.revertedWithCustomError(vault, "InvalidPublicKeyLength")
+        .withArgs(invalidSourcePubkey);
+    });
+
+    it("Should revert if target pubkey is not 48 bytes", async function () {
+      const validSourcePubkey = "0x" + "1".repeat(96); // 48 bytes
+      // Invalid target pubkey (only 2 bytes)
+      const invalidTargetPubkey = "0x5678";
+
+      const fee = await getConsolidationFee();
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests([validSourcePubkey], [invalidTargetPubkey], { value: fee }),
+      )
+        .to.be.revertedWithCustomError(vault, "InvalidPublicKeyLength")
+        .withArgs(invalidTargetPubkey);
+    });
+
+    it("Should revert if addition fails at the consolidation request contract", async function () {
+      const { sourcePubkeysHexArray, targetPubkeysHexArray } = generateConsolidationRequestPayload(1);
+      const fee = await getConsolidationFee();
+
+      // Set mock to fail on add
+      await consolidationPredeployed.mock__setFailOnAddRequest(true);
+
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: fee }),
+      ).to.be.revertedWithCustomError(vault, "RequestAdditionFailed");
+    });
+
+    it("Should revert when fee read fails", async function () {
+      await consolidationPredeployed.mock__setFailOnGetFee(true);
+
+      const { sourcePubkeysHexArray, targetPubkeysHexArray } = generateConsolidationRequestPayload(2);
+      const fee = 10n;
+
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: fee }),
+      ).to.be.revertedWithCustomError(vault, "FeeReadFailed");
+    });
+
+    it("Should revert when the provided fee exceeds the required amount", async function () {
+      const requestCount = 3;
+      const { sourcePubkeysHexArray, targetPubkeysHexArray } = generateConsolidationRequestPayload(requestCount);
+
+      const fee = 3n;
+      await consolidationPredeployed.mock__setFee(fee);
+      const consolidationFee = 9n + 1n; // 3 request * 3 gwei (fee) + 1 gwei (extra fee)= 10 gwei
+
+      await expect(
+        vault
+          .connect(consolidationGateway)
+          .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: consolidationFee }),
+      )
+        .to.be.revertedWithCustomError(vault, "IncorrectFee")
+        .withArgs(9n, 10n);
+    });
+
+    ["0x", "0x01", "0x" + "0".repeat(61) + "1", "0x" + "0".repeat(65) + "1"].forEach((unexpectedFee) => {
+      it(`Should revert if unexpected fee value ${unexpectedFee} is returned`, async function () {
+        await consolidationPredeployed.mock__setFeeRaw(unexpectedFee);
+
+        const { sourcePubkeysHexArray, targetPubkeysHexArray } = generateConsolidationRequestPayload(1);
+        const fee = 10n;
+
+        await expect(
+          vault
+            .connect(consolidationGateway)
+            .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: fee }),
+        ).to.be.revertedWithCustomError(vault, "FeeInvalidData");
+      });
+    });
+
+    it("Should accept consolidation requests when the provided fee matches the exact required amount", async function () {
+      const requestCount = 3;
+      const { sourcePubkeysHexArray, sourcePubkeys, targetPubkeysHexArray, targetPubkeys } =
+        generateConsolidationRequestPayload(requestCount);
+
+      const fee = 3n;
+      await consolidationPredeployed.mock__setFee(3n);
+      const expectedTotalConsolidationFee = 9n;
+
+      await testEIP7251Mock(
+        () =>
+          vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, {
+            value: expectedTotalConsolidationFee,
+          }),
+        sourcePubkeys,
+        targetPubkeys,
+        fee,
+      );
+
+      // Check extremely high fee
+      const highFee = ethers.parseEther("10");
+      await consolidationPredeployed.mock__setFee(highFee);
+      const expectedLargeTotalConsolidationFee = ethers.parseEther("30");
+
+      await testEIP7251Mock(
+        () =>
+          vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, {
+            value: expectedLargeTotalConsolidationFee,
+          }),
+        sourcePubkeys,
+        targetPubkeys,
+        highFee,
+      );
+    });
+
+    it("Should emit consolidation event", async function () {
+      const requestCount = 3;
+      const { sourcePubkeysHexArray, sourcePubkeys, targetPubkeysHexArray, targetPubkeys } =
+        generateConsolidationRequestPayload(requestCount);
+
+      const fee = 3n;
+      await consolidationPredeployed.mock__setFee(fee);
+      const expectedTotalConsolidationFee = 9n; // 3 requests * 3 gwei (fee) = 9 gwei
+
+      await expect(
+        vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, {
+          value: expectedTotalConsolidationFee,
+        }),
+      )
+        .to.emit(vault, "ConsolidationRequestAdded")
+        .withArgs(encodeEIP7251Payload(sourcePubkeys[0], targetPubkeys[0]))
+        .and.to.emit(vault, "ConsolidationRequestAdded")
+        .withArgs(encodeEIP7251Payload(sourcePubkeys[1], targetPubkeys[1]))
+        .and.to.emit(vault, "ConsolidationRequestAdded")
+        .withArgs(encodeEIP7251Payload(sourcePubkeys[2], targetPubkeys[2]));
+    });
+
+    it("Should not affect contract balance", async function () {
+      const requestCount = 3;
+      const { sourcePubkeysHexArray, sourcePubkeys, targetPubkeysHexArray, targetPubkeys } =
+        generateConsolidationRequestPayload(requestCount);
+
+      const fee = 3n;
+      await consolidationPredeployed.mock__setFee(fee);
+      const expectedTotalConsolidationFee = 9n; // 3 requests * 3 gwei (fee) = 9 gwei
+
+      const initialBalance = await getWithdrawalCredentialsContractBalance();
+
+      await testEIP7251Mock(
+        () =>
+          vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, {
+            value: expectedTotalConsolidationFee,
+          }),
+        sourcePubkeys,
+        targetPubkeys,
+        fee,
+      );
+      expect(await getWithdrawalCredentialsContractBalance()).to.equal(initialBalance);
+    });
+
+    it("Should transfer the total calculated fee to the EIP-7251 consolidation contract", async function () {
+      const requestCount = 3;
+      const { sourcePubkeysHexArray, sourcePubkeys, targetPubkeysHexArray, targetPubkeys } =
+        generateConsolidationRequestPayload(requestCount);
+
+      const fee = 3n;
+      await consolidationPredeployed.mock__setFee(3n);
+      const expectedTotalConsolidationFee = 9n;
+
+      const initialBalance = await getConsolidationPredeployedContractBalance();
+      await testEIP7251Mock(
+        () =>
+          vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, {
+            value: expectedTotalConsolidationFee,
+          }),
+        sourcePubkeys,
+        targetPubkeys,
+        fee,
+      );
+
+      expect(await getConsolidationPredeployedContractBalance()).to.equal(
+        initialBalance + expectedTotalConsolidationFee,
+      );
+    });
+
+    it("Should ensure consolidation requests are encoded as expected with a 96-byte pubkeys ", async function () {
+      const requestCount = 16;
+      const { sourcePubkeysHexArray, sourcePubkeys, targetPubkeysHexArray, targetPubkeys } =
+        generateConsolidationRequestPayload(requestCount);
+
+      const tx = await vault
+        .connect(consolidationGateway)
+        .addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, { value: 16n });
+
+      const receipt = await tx.wait();
+
+      const events = findEIP7251MockEvents(receipt!);
+      expect(events.length).to.equal(requestCount);
+
+      for (let i = 0; i < requestCount; i++) {
+        const encodedRequest = events[i].args[0];
+        // 0x (2 characters) + 48-byte pubkey (96 characters) + 48-byte pubkey (96 characters) = 194 characters
+        expect(encodedRequest.length).to.equal(194);
+
+        expect(encodedRequest.slice(0, 2)).to.equal("0x");
+        expect(encodedRequest.slice(2, 98)).to.equal(sourcePubkeys[i]);
+        expect(encodedRequest.slice(98, 194)).to.equal(targetPubkeys[i]);
+      }
+    });
+
+    const testCasesForConsolidationRequests = [
+      { requestCount: 1 },
+      { requestCount: 3 },
+      { requestCount: 7 },
+      { requestCount: 10 },
+      { requestCount: 100 },
+    ];
+
+    testCasesForConsolidationRequests.forEach(({ requestCount }) => {
+      it(`Should process ${requestCount} consolidation request(s) successfully`, async function () {
+        const { sourcePubkeysHexArray, sourcePubkeys, targetPubkeysHexArray, targetPubkeys } =
+          generateConsolidationRequestPayload(requestCount);
+
+        const fee = 1n;
+        const expectedTotalConsolidationFee = BigInt(requestCount) * fee;
+
+        await testEIP7251Mock(
+          () =>
+            vault.connect(consolidationGateway).addConsolidationRequests(sourcePubkeysHexArray, targetPubkeysHexArray, {
+              value: expectedTotalConsolidationFee,
+            }),
+          sourcePubkeys,
+          targetPubkeys,
+          fee,
         );
       });
     });
