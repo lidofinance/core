3642a7a5c9dacd34ebe9041e27037c3299d18dac
Aleksei Potapkin <folkyatina@gmail.com>
2025-11-07 20:50:00 +0200

    fix(VaultHub): add early return for small values in shortfall

--- contracts/0.8.25/vaults/VaultHub.sol
+++ contracts/0.8.25/vaults/VaultHub.sol
@@ -1218,6 +1218,9 @@ contract VaultHub is PausableUntilWithRoles {
             return 0;
         }
 
+        // if not healthy and low in debt, please rebalance the whole amount
+        if (liabilityShares_ <= 100) return liabilityShares_;
+
         uint256 reserveRatioBP = _connection.reserveRatioBP;
         uint256 maxMintableRatio = (TOTAL_BASIS_POINTS - reserveRatioBP);
         uint256 liability = _getPooledEthBySharesRoundUp(liabilityShares_);
