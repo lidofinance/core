1364ddd5e8c8dcaa2c78dc63bc87e31fc9a6175e
failingtwice <azatsdev@gmail.com>
2025-11-26 13:38:46 +0500

    feat: update deploy params

--- lib/config-schemas.ts
+++ lib/config-schemas.ts
@@ -81,10 +81,6 @@ const TriggerableWithdrawalsGatewaySchema = z.object({
 
 // Easy track schema
 const EasyTrackSchema = z.object({
-  trustedCaller: EthereumAddressSchema,
-  initialValidatorExitFeeLimit: BigIntStringSchema,
-  maxGroupShareLimit: BigIntStringSchema,
-  maxDefaultTierShareLimit: NonNegativeIntSchema,
   VaultsAdapter: EthereumAddressSchema,
   newFactories: z.object({
     AlterTiersInOperatorGrid: EthereumAddressSchema,
