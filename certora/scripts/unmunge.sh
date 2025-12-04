0e6e3cc15ddb53b3eb0ff35fa634c21af62f4d2a
lilCertora <lilian@certora.com>
2025-12-04 15:16:28 +0100

    munges modifs

--- certora/scripts/unmunge.sh
+++ certora/scripts/unmunge.sh
@@ -1,4 +1,2 @@
 git apply -R ./certora/munges/munge-strategy-lib.patch
 git apply -R ./certora/munges/munge-total-shares-access.patch
-#git apply -R ./certora/munges/munge-IStakingVault-path-safe-assembly.patch
-#git apply -R ./certora/munges/munge-safe-assembly.patch
