f37bb83ff25886cded7527e1d7a0a16547ab4231
lilCertora <lilian@certora.com>
2025-12-05 20:01:52 +0100

    renaming munges to patches

--- certora/scripts/munge.sh
+++
@@ -1,2 +0,0 @@
-git apply ./certora/munges/munge-strategy-lib.patch
-git apply ./certora/munges/munge-total-shares-access.patch
--- certora/scripts/unmunge.sh
+++
@@ -1,2 +0,0 @@
-git apply -R ./certora/munges/munge-strategy-lib.patch
-git apply -R ./certora/munges/munge-total-shares-access.patch
