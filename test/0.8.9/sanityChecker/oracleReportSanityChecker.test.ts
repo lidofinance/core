fc83550fbf52f92a898b30210a3320d79b474e18
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2026-03-13 13:52:02 +0000

    fix: add missing consolidationGateway to test

--- test/0.8.9/sanityChecker/oracleReportSanityChecker.test.ts
+++ test/0.8.9/sanityChecker/oracleReportSanityChecker.test.ts
@@ -1529,6 +1529,7 @@ describe("OracleReportSanityChecker.sol", () => {
           oracleDaemonConfig: deployer.address,
           validatorExitDelayVerifier: deployer.address,
           triggerableWithdrawalsGateway: deployer.address,
+          consolidationGateway: deployer.address,
           accounting: await accounting.getAddress(),
           predepositGuarantee: deployer.address,
           wstETH: deployer.address,
