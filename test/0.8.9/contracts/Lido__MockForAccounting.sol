942c17e8897f54189e7d0d36e8d4f68847d7f21b
Maksim Kuraian <kuraian@proton.me>
2026-03-16 18:06:12 +0100

    chore: remove deprecated event

--- test/0.8.9/contracts/Lido__MockForAccounting.sol
+++ test/0.8.9/contracts/Lido__MockForAccounting.sol
@@ -11,10 +11,6 @@ contract Lido__MockForAccounting {
     uint256 public reportClPendingBalance;
     uint256 public depositedBalance;
 
-    // Emitted when validators number delivered by the oracle
-    // @deprecated This event is deprecated. Use CLBalancesUpdated instead for balance-based accounting
-    event CLValidatorsUpdated(uint256 indexed reportTimestamp, uint256 preCLValidators, uint256 postCLValidators);
-
     // Emitted when CL balances are updated by the oracle
     event CLBalancesUpdated(uint256 indexed reportTimestamp, uint256 clValidatorsBalance, uint256 clPendingBalance);
     event Mock__CollectRewardsAndProcessWithdrawals(
