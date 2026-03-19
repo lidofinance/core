3fe6ebafc32dd3f407a7e844a33d93f2c2e65ca0
Anna Mukharram <amuhar3@gmail.com>
2026-03-19 16:54:52 +0400

    fix: import

--- contracts/common/interfaces/TopUpWitness.sol
+++ contracts/common/interfaces/TopUpWitness.sol
@@ -4,6 +4,8 @@
 // solhint-disable-next-line
 pragma solidity >=0.8.9;
 
+import {BeaconRootData, ValidatorWitness} from "contracts/common/interfaces/ValidatorWitness.sol";
+
 struct TopUpData {
     uint256 moduleId;
     uint256[] keyIndices;
@@ -12,23 +14,4 @@ struct TopUpData {
     BeaconRootData beaconRootData;
     ValidatorWitness[] validatorWitness;
     uint256[] pendingBalanceGwei;
-}
-
-struct BeaconRootData {
-    uint64 childBlockTimestamp; // for EIP-4788 lookup
-    uint64 slot; // header slot
-    uint64 proposerIndex; // header proposer
-}
-
-struct ValidatorWitness {
-    // Merkle path: Validator[i] → … → state_root → beacon_block_root
-    bytes32[] proofValidator;
-    // Full Validator container fields (minus WC)
-    bytes pubkey;
-    uint64 effectiveBalance;
-    uint64 activationEligibilityEpoch;
-    uint64 activationEpoch;
-    uint64 exitEpoch;
-    uint64 withdrawableEpoch;
-    bool slashed;
 }
