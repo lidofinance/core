c2bba74d353bf598d1dd66efa1c1618ed7369a58
KRogLA <defacec0de@gmail.com>
2026-04-21 08:28:17 +0200

    test: add signer helper, refactor mock scripts

--- scripts/upgrade/steps/1000-devnet-new-vote.ts
+++ scripts/upgrade/steps/1000-devnet-new-vote.ts
@@ -1,9 +1,10 @@
-import { ethers } from "hardhat";
 import { mockAragonVoting } from "scripts/utils/upgrade";
+
+import { ether, getSignerOrImpersonate } from "lib";
 
 export async function main() {
   const holderAddress = process.env.HOLDER || process.env.DEPLOYER || "";
-  const holder = await ethers.provider.getSigner(holderAddress);
+  const holder = await getSignerOrImpersonate(holderAddress, ether("100"));
   const voteDescription = process.env.VOTE_DESCRIPTION || "vote-description";
   const voteId = BigInt(process.env.VOTE_ID || "");
 
