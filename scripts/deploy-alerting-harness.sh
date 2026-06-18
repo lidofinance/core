bbb34af2e7b2943058cfce52e9dffd0a4bc3aca6
KRogLA <defacec0de@gmail.com>
2026-06-18 04:01:57 +0200

    fix: steps path

--- scripts/deploy-alerting-harness.sh
+++ scripts/deploy-alerting-harness.sh
@@ -2,6 +2,6 @@
 set -e +u
 set -o pipefail
 
-export STEPS_FILE=harness/steps-deploy-alerting-harness.json
+export STEPS_FILE=tooling/steps-deploy-alerting-harness.json
 
 bash scripts/dao-deploy.sh
